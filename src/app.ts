2f5a3bfd9cd5703eec40850c3d63a1460bb6c556
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-02-06 16:14:08 +0000

    VIA-5 AJ/DB/MD Adding jest testing framework

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,10 @@ const __filename = fileURLToPath(import.meta.url); // get the resolved path to t
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app: Express = express();
-const port = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", IndexRouter);
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+export default app;
---
+++ tests/index.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import app from '../src/app';
+import { describe, expect, it } from "@jest/globals";
+
+describe('Index Controller', () => {
+  it('GET / should return our home page', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
