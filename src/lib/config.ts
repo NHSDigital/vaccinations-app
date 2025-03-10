42ecefe2e10012f21e40f50b801ab34115b83eef
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-03-10 12:23:30 +0000

    VIA-52 DB/AS Remove default env var values

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,6 @@
 const configProvider = () => ({
-  CONTENT_API_ENDPOINT: process.env.CONTENT_API_ENDPOINT || "",
-  CONTENT_API_VACCINATIONS_PATH:
-    process.env.CONTENT_API_VACCINATIONS_PATH || "",
+  CONTENT_API_ENDPOINT: process.env.CONTENT_API_ENDPOINT,
+  CONTENT_API_VACCINATIONS_PATH: process.env.CONTENT_API_VACCINATIONS_PATH,
 });
 
 export default configProvider;
