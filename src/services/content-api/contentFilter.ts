bc2791253d0c93199b470e2a02bbca9a17e92a55
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-03-12 13:47:39 +0000

    VIA-52 Refactor ContentFilter methods for reusability

--- src/services/content-api/contentFilter.ts
+++ src/services/content-api/contentFilter.ts
@@ -1,6 +1,8 @@
 import { getContentForVaccine } from "@src/services/content-api/contentService";
 import { VaccineTypes } from "@src/models/vaccine";
 
+type Aspect = "OverviewHealthAspect" | "BenefitsHealthAspect";
+
 type ContentApiHasPartSubsection = {
   text: string;
   type: string;
@@ -20,33 +22,49 @@ type ContentApiVaccineResponse = {
   hasPart: ContentApiHasPart[];
 };
 
-const extractTextFromAllPartsForAspect = (
+const findAspect = (
   contentApiVaccineText: ContentApiVaccineResponse,
-  aspect: string,
+  aspect: Aspect,
 ) => {
   const aspectInfo = contentApiVaccineText.hasPart.find(
     (part: ContentApiHasPart) => part.hasHealthAspect.endsWith(aspect),
   );
+  return aspectInfo;
+};
+
+const extractAllPartsTextForAspect = (
+  contentApiVaccineText: ContentApiVaccineResponse,
+  aspect: Aspect,
+) => {
+  const aspectInfo = findAspect(contentApiVaccineText, aspect);
   const aspectText = aspectInfo!.hasPart
     .map((part: ContentApiHasPartSubsection) => part.text)
     .join("");
   return aspectText;
 };
 
+const extractDescriptionForAspect = (
+  contentApiVaccineText: ContentApiVaccineResponse,
+  aspect: Aspect,
+) => {
+  const aspectInfo = findAspect(contentApiVaccineText, aspect);
+  return aspectInfo!.description;
+};
+
 const getPageCopyForVaccine = async (vaccineName: VaccineTypes) => {
   const contentApiVaccineText = await getContentForVaccine(vaccineName);
-  const description = contentApiVaccineText.hasPart.find(
-    (part: ContentApiHasPart) =>
-      part.hasHealthAspect.endsWith("OverviewHealthAspect"),
-  ).description;
 
-  const whatVaccineIsFor = extractTextFromAllPartsForAspect(
+  const overview = extractDescriptionForAspect(
+    contentApiVaccineText,
+    "OverviewHealthAspect",
+  );
+  const whatVaccineIsFor = extractAllPartsTextForAspect(
     contentApiVaccineText,
     "BenefitsHealthAspect",
   );
 
   return {
-    overview: description,
+    overview,
     whatVaccineIsFor,
   };
 };
