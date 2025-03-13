85d7f336e0978515eb46a801b4ea48cfb187c5a9
Marie Dedikova <197628467+marie-dedikova-nhs@users.noreply.github.com>
2025-03-13 15:13:18 +0000

    VIA-23 MD/AJ Finish vaccination hub page incl. styling

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -2,13 +2,83 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "@jest/globals";
 import VaccinationsHub from "./page";
+import { getContent } from "@src/services/content-api/contentService";
+import { JSX } from "react";
 
-describe("Page", () => {
-  it("renders a heading", () => {
-    render(<VaccinationsHub />);
+jest.mock("@src/services/content-api/contentService");
 
-    const heading = screen.getByRole("heading", { name: "Vaccinations hub" });
+describe("Vaccination Hub Page", () => {
+  const mockHeading: string = "Vaccinations";
+  const mockTextUnderHeading: string =
+    "Find out about vaccinations for babies," +
+    " children and adults, including why they're important and how to get them.";
+
+  beforeEach(() => {
+    const mockContent = {
+      about: { name: mockHeading },
+      description: mockTextUnderHeading,
+    };
+    (getContent as jest.Mock).mockResolvedValue(mockContent);
+  });
+
+  it("renders all headings", async () => {
+    const VaccinationsHubPage: JSX.Element = await VaccinationsHub();
+    render(VaccinationsHubPage);
+
+    const headings: HTMLElement[] = screen.getAllByRole("heading", {
+      name: mockHeading,
+    });
+
+    expect(headings.length).toBe(2);
+  });
+
+  it("renders the text below heading", async () => {
+    const VaccinationsHubPage: JSX.Element = await VaccinationsHub();
+    render(VaccinationsHubPage);
+
+    const heading: HTMLElement = screen.getByText(mockTextUnderHeading);
 
     expect(heading).toBeInTheDocument();
+  });
+
+  it("renders View all link", async () => {
+    const VaccinationsHubPage: JSX.Element = await VaccinationsHub();
+    render(VaccinationsHubPage);
+
+    const link: HTMLElement = screen.getByRole("link", { name: "View all" });
+
+    expect(link).toBeInTheDocument();
+  });
+
+  it("renders the text below 2nd heading", async () => {
+    const VaccinationsHubPage: JSX.Element = await VaccinationsHub();
+    render(VaccinationsHubPage);
+
+    const heading: HTMLElement = screen.getByText(
+      "Based on your age, these vaccinations may be relevant - some" +
+        " vaccinations may not be needed or have already been given.",
+    );
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders 6-in-1 link", async () => {
+    const VaccinationsHubPage: JSX.Element = await VaccinationsHub();
+    render(VaccinationsHubPage);
+
+    const link: HTMLElement = screen.getByRole("link", {
+      name: "6-in-1 vaccine",
+    });
+
+    expect(link).toBeInTheDocument();
+  });
+
+  it("renders RSV vaccine link", async () => {
+    const VaccinationsHubPage: JSX.Element = await VaccinationsHub();
+    render(VaccinationsHubPage);
+
+    const link: HTMLElement = screen.getByRole("link", { name: "RSV vaccine" });
+
+    expect(link).toBeInTheDocument();
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
-import type { Metadata } from "next";
+import { getContent } from "@src/services/content-api/contentService";
+import CardLink from "@src/app/_components/nhs-app/CardLink";
+import styles from "./styles.module.css";
+import { JSX } from "react";
 
-export const metadata: Metadata = {
-  title: "Vaccinations hub",
-};
+export const dynamic = "force-dynamic";
 
-const VaccinationsHub = () => {
+const VaccinationsHub = async (): Promise<JSX.Element> => {
+  const content = await getContent();
+
   return (
     <div>
-      <h1>{metadata.title as string}</h1>
-      <a href="/schedule">(placeholder) View all vaccinations</a>
+      <h1>{content.about.name}</h1>
+      <p>{content.description}</p>
+      <div className={styles.subheading}>
+        <div>
+          <h3>{content.about.name}</h3>
+        </div>
+        <div>
+          <a href="/schedule">View all</a>
+        </div>
+      </div>
+      <p>
+        Based on your age, these vaccinations may be relevant - some
+        vaccinations may not be needed or have already been given.
+      </p>
+      <CardLink title={"6-in-1 vaccine"} link={"/vaccines/6-in-1"} />
+      <CardLink title={"RSV vaccine"} link={"/vaccines/rsv"} />
     </div>
   );
 };
