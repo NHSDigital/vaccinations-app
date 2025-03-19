27754faf768cd7d2f538983b9658bd1249e7d953
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-03-19 15:07:48 +0000

    VIA-52 MD/AS Modify vaccine page to pull content from styling service

--- src/app/_components/vaccine/vaccine.tsx
+++ src/app/_components/vaccine/vaccine.tsx
@@ -1,7 +1,7 @@
 import { JSX } from "react";
-import { getPageCopyForVaccine } from "@src/services/content-api/contentFilter";
 import { VaccineTypes } from "@src/models/vaccine";
 import Details from "@src/app/_components/nhs-frontend/Details";
+import { getStyledContentForVaccine } from "@src/services/content-api/contentStylingService";
 
 interface VaccineProps {
   name: string;
@@ -9,29 +9,29 @@ interface VaccineProps {
 }
 
 const Vaccine = async (props: VaccineProps): Promise<JSX.Element> => {
-  const content = await getPageCopyForVaccine(props.vaccine);
+  const styledContent = await getStyledContentForVaccine(props.vaccine);
 
   return (
     <div>
       <h1 className="app-dynamic-page-title__heading">{props.name} Vaccine</h1>
-      <p>{content.overview}</p>
+      <p>{styledContent.overview}</p>
 
       <h2 className="nhsuk-heading-s">More information</h2>
       <div className="nhsuk-expander-group">
         <Details
-          summaryText={content.whatVaccineIsFor.heading}
-          text={content.whatVaccineIsFor.text}
+          title={styledContent.whatVaccineIsFor.heading}
+          component={styledContent.whatVaccineIsFor.component}
         />
         <Details
-          summaryText={content.whoVaccineIsFor.heading}
-          text={content.whoVaccineIsFor.text}
+          title={styledContent.whoVaccineIsFor.heading}
+          component={styledContent.whoVaccineIsFor.component}
         />
         <Details
-          summaryText={content.howToGetVaccine.heading}
-          text={content.howToGetVaccine.text}
+          title={styledContent.howToGetVaccine.heading}
+          component={styledContent.howToGetVaccine.component}
         />
       </div>
-      <a href={content.webpageLink}>
+      <a href={styledContent.webpageLink}>
         Find out more about {props.name} vaccination on the NHS.uk
       </a>
     </div>
