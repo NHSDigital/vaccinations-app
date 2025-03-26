f2a96839bad6383124945be330849b6b0ed630e6
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-03-26 14:04:01 +0000

    VIA-102 AJ/AS Added open-next terraform module to deploy the app (WIP)

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -8,6 +8,11 @@ variable "project_identifier" {
   description = "The project identifier, typically the repository name"
 }
 
+variable "project_identifier_shortcode" {
+  type        = string
+  description = "The project identifier shortcode to be used a prefixes/suffixes"
+}
+
 variable environment {
   type        = string
   description = "The AWS environment (account) to deploy to, e.g. dev/test/preprod/prod"
