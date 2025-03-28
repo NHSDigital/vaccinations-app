119eb440a42bdadd1a7da776450255f0d70f2ea7
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-03-28 13:36:03 +0000

    VIA-129 Add integration test for Content API classes
    Covers all components realted to calling the content API, from the entry
    point used by services to the API call itself and all post-processing
    steps

---
+++ src/services/content-api/content_api.integration.test.tsx
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment node
+ */
+
+import {
+  getStyledContentForVaccine,
+  StyledVaccineContent,
+} from "@src/services/content-api/contentStylingService";
+import { VaccineTypes } from "@src/models/vaccine";
+import { genericVaccineContentAPIResponse } from "@test-data/content-api/data";
+
+jest.mock("@src/utils/config", () => () => ({
+  CONTENT_API_ENDPOINT: "https://content-endpoint",
+}));
+
+const fetchMock = jest.spyOn(global, "fetch").mockImplementation(
+  jest.fn(() =>
+    Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve(genericVaccineContentAPIResponse),
+    }),
+  ) as jest.Mock,
+);
+
+describe("Content API Integration Test ", () => {
+  it("should return processed data from external Content API", async () => {
+    const styledVaccineContent: StyledVaccineContent =
+      await getStyledContentForVaccine(VaccineTypes.RSV);
+
+    expect(fetchMock).toHaveBeenCalled();
+    expect(styledVaccineContent).not.toBeNull();
+    expect(styledVaccineContent.overview).toEqual(
+      genericVaccineContentAPIResponse.mainEntityOfPage[0].text,
+    );
+    expect(styledVaccineContent.webpageLink).toEqual(
+      genericVaccineContentAPIResponse.webpage,
+    );
+  });
+});
