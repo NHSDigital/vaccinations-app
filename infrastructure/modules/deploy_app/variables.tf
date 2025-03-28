4901265e9d1bbe074b6153ed1418d3fb3d93c738
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-03-28 09:48:42 +0000

    VIA-102 AJ/AS Refactoring to look prettier

--- infrastructure/modules/deploy_app/variables.tf
+++ infrastructure/modules/deploy_app/variables.tf
@@ -1,3 +1,14 @@
-variable "prefix" {}
-variable "open-next-path" {}
-variable "default_tags" {}
+variable "prefix" {
+  type        = string
+  description = "Prefix to be applied to resources created"
+}
+
+variable "open-next-path" {
+  type        = string
+  description = "Relative path to open next output directory"
+}
+
+variable "default_tags" {
+  type        = string
+  description = "Map of default key-value pair of tags to add to resources"
+}
