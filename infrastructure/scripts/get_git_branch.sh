5e3e532861cd37fa3d2c6a41d55ccb28932098d3
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-02 17:34:30 +0100

    VIA-102 AJ/AS Reducing the size of branch name to accommodate max length

--- infrastructure/scripts/get_git_branch.sh
+++ infrastructure/scripts/get_git_branch.sh
@@ -2,4 +2,4 @@
 
 RAW_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 BRANCH_NAME=$(echo "$RAW_BRANCH_NAME" | sed 's|/|-|g' | tr '[:upper:]' '[:lower:]')
-echo "{\"output\": \"${BRANCH_NAME:0:10}\"}"
+echo "{\"output\": \"${BRANCH_NAME:0:8}\"}"
