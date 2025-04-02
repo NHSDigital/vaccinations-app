633c4c07a527f70b76ab597b869ff524920e858a
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-02 12:10:23 +0100

    VIA-102 AJ/AS Get environment from AWS SSM

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,7 +6,11 @@ const configProvider = () => ({
 });
 
 const getFromEnvironmentOrSSM = (param: string): string => {
-  return process.env[param] || getSSMParam(param);
+  const value: string | undefined = process.env[param] || getSSMParam(param);
+  if (value === undefined || value === null) {
+    throw new Error(`Unable to get param: ${param}`);
+  }
+  return value;
 };
 
 export default configProvider;
--- src/utils/get-ssm-param.ts
+++ src/utils/get-ssm-param.ts
@@ -1,5 +1,33 @@
-const getSSMParam = (param: string): string => {
-  return param;
+import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
+import type { GetParameterCommandOutput } from "@aws-sdk/client-ssm";
+
+export const AWS_PRIMARY_REGION = "eu-west-2";
+
+const getSSMParam = (name: string): string | undefined => {
+  const client = new SSMClient({
+    region: AWS_PRIMARY_REGION,
+  });
+
+  const command = new GetParameterCommand({
+    Name: name,
+    WithDecryption: true,
+  });
+
+  let value: string | undefined = undefined;
+  client
+    .send(command)
+    .then((result: GetParameterCommandOutput) => {
+      if (result && result.Parameter) {
+        value = result.Parameter.Value;
+      } else {
+        console.error(`Could not find SSM parameter ${name}`);
+      }
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+
+  return value;
 };
 
 export default getSSMParam;
