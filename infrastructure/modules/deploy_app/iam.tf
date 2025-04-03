a155cedb9a560c219bd36e37fc048fcf40a4dffc
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-03 18:33:26 +0100

    VIA-102 AJ/AS Fixed issue with templatefile

--- infrastructure/modules/deploy_app/iam.tf
+++ infrastructure/modules/deploy_app/iam.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_policy" "server_lambda_additional_policy" {
   name   = "${var.prefix}-server-lambda-additional-policy"
-  policy = templatefile("${path.module}/iam-policy.json")
+  policy = templatefile("${path.module}/iam-policy.json", {})
 }
