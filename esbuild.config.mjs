06b6587e5abc24246879bdb1136975a3fee2264c
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-11 10:50:28 +0100

    VIA-138 AJ Build, package and upload lambda

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,27 +1,30 @@
 import * as esbuild from "esbuild";
-import * as child_process from "node:child_process";
+import * as fs from "node:fs";
 
-const LAMBDA_PATH = "dist/lambda.js";
+const OUTPUT_DIR = "dist";
 
-const build = async () => {
+const rmDistDirectory = async () => {
+  fs.rm(OUTPUT_DIR, { recursive: true, force: true }, (err) => {
+    if (err) throw err;
+    console.log(`Directory '${OUTPUT_DIR}' deleted!`);
+  });
+};
+
+const buildLambda = async () => {
   await esbuild.build({
     entryPoints: ["src/_lambda/content-cache-hydrator/handler.ts"],
     bundle: true,
     minify: true,
     platform: "node",
     target: "node22",
-    outfile: LAMBDA_PATH
+    outfile: `${OUTPUT_DIR}/lambda.js`
   });
-}
-
-const createZip = () => {
-  child_process.exec(`zip -j dist/lambda.zip ${LAMBDA_PATH}`)
-}
+};
 
 try {
-  await build();
-  createZip();
-  console.log("Built lambda successfully!");
+  await rmDistDirectory();
+  await buildLambda();
+  console.log(`Built lambda successfully -> ${OUTPUT_DIR}/lambda.js`);
 } catch (e) {
   console.error("Building lambda failed: ", e);
 }
