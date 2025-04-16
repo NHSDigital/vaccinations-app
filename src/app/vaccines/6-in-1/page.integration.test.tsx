f3c970db2907a93e7e595df09d43cd3fe177b76a
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-16 14:30:52 +0100

    VIA-138 AJ/MD Fixed page integration test

--- src/app/vaccines/6-in-1/page.integration.test.tsx
+++ src/app/vaccines/6-in-1/page.integration.test.tsx
@@ -1,30 +1,29 @@
+import { configProvider } from "@src/utils/config";
 import { render, screen } from "@testing-library/react";
 import Vaccine6in1 from "@src/app/vaccines/6-in-1/page";
-import { getContentForVaccine } from "@src/services/content-api/gateway/content-reader-service";
-import { mockApiContent } from "@test-data/content-api/data";
 import { act } from "react";
+import mockSixInOneVaccineContent from "@project/wiremock/__files/6-in-1-vaccine.json";
 
-jest.mock("@src/services/content-api/gateway/content-reader-service");
+jest.mock("@src/utils/config");
 
 describe("6-in-1 vaccine page - integration test", () => {
-  beforeEach(() => {
-    (getContentForVaccine as jest.Mock).mockResolvedValue(
-      mockApiContent,
-    );
-  });
+  (configProvider as jest.Mock).mockImplementation(() => ({
+    CONTENT_CACHE_PATH: "wiremock/__files/",
+    PINO_LOG_LEVEL: "info"
+  }));
 
   it("should display 6-in-1 content from embedded vaccine component", async () => {
     await act(async () => {
       render(Vaccine6in1());
     });
 
-    const sixInOneHeading: HTMLElement = screen.getByRole("heading", {
+    const sixInOneHeading: HTMLElement = await screen.findByRole("heading", {
       level: 1,
-      name: `6-in-1 Vaccine`,
+      name: `6-in-1 vaccine`,
     });
     expect(sixInOneHeading).toBeInTheDocument();
-    expect(screen.getByTestId("overview-text")).toHaveTextContent(
-      mockApiContent.mainEntityOfPage[0].text,
-    );
+
+    const overview = await screen.findByTestId("overview-text");
+    expect(overview).toHaveTextContent(mockSixInOneVaccineContent.mainEntityOfPage[0].text as string);
   });
 });
