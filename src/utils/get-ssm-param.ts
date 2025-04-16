e309d809ea43dbfe666400b12eb0558bb26ff9c4
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-16 09:02:40 +0100

    VIA-138 AJ/MD Adding error handling in getting configuration from SSM

--- src/utils/get-ssm-param.ts
+++ src/utils/get-ssm-param.ts
@@ -6,20 +6,24 @@ import { logger } from "@src/utils/logger";
 const log = logger.child({ module: "get-ssm-param" });
 
 const getSSMParam = async (name: string): Promise<string | undefined> => {
-  const client = new SSMClient({
-    region: AWS_PRIMARY_REGION,
-  });
+  try {
+    const client = new SSMClient({
+      region: AWS_PRIMARY_REGION,
+    });
 
-  const command = new GetParameterCommand({
-    Name: name,
-    WithDecryption: true,
-  });
+    const command = new GetParameterCommand({
+      Name: name,
+      WithDecryption: true,
+    });
 
-  const response: GetParameterCommandOutput = await client.send(command);
-  if (response.$metadata.httpStatusCode === 200) {
-    return response.Parameter?.Value;
-  } else {
-    log.error(response.$metadata);
+    const response: GetParameterCommandOutput = await client.send(command);
+    if (response.$metadata.httpStatusCode === 200) {
+      return response.Parameter?.Value;
+    } else {
+      log.error(`Error GetParameterCommand response: ${response.$metadata}`);
+    }
+  } catch (error) {
+    log.error(`Error getSSMParam: ${error}`);
   }
 
   return undefined;
