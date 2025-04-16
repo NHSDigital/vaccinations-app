9051f84846ebc812338295520ecbb3baa9f1fc6c
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-16 14:33:59 +0100

    VIA-138 AJ/MD Fixed rsv page integration test

--- src/app/vaccines/rsv/page.integration.test.tsx
+++ src/app/vaccines/rsv/page.integration.test.tsx
@@ -1,30 +1,31 @@
+import { configProvider } from "@src/utils/config";
 import { render, screen } from "@testing-library/react";
 import VaccineRsv from "@src/app/vaccines/rsv/page";
-import { getStyledContentForVaccine } from "@src/services/content-api/parsers/content-styling-service";
-import { mockStyledContent } from "@test-data/content-api/data";
 import { act } from "react";
+import mockRsvVaccineContent from "@project/wiremock/__files/rsv-vaccine.json";
 
-jest.mock("@src/services/content-api/parsers/content-styling-service.tsx");
+jest.mock("@src/utils/config");
 
 describe("RSV vaccine page - integration test", () => {
-  beforeEach(() => {
-    (getStyledContentForVaccine as jest.Mock).mockResolvedValue(
-      mockStyledContent,
-    );
-  });
+  (configProvider as jest.Mock).mockImplementation(() => ({
+    CONTENT_CACHE_PATH: "wiremock/__files/",
+    PINO_LOG_LEVEL: "info"
+  }));
 
   it("should display RSV content from embedded vaccine component", async () => {
     await act(async () => {
       render(VaccineRsv());
     });
 
-    const rsvHeading: HTMLElement = screen.getByRole("heading", {
+    const rsvHeading: HTMLElement = await screen.findByRole("heading", {
       level: 1,
       name: `RSV vaccine`,
     });
     expect(rsvHeading).toBeInTheDocument();
-    expect(screen.getByTestId("overview-text")).toHaveTextContent(
-      mockStyledContent.overview,
+
+    const overview = await screen.findByTestId("overview-text");
+    expect(overview).toHaveTextContent(
+      mockRsvVaccineContent.mainEntityOfPage[0].text as string,
     );
   });
 });
