74f52fb3edd0f1f175d1c95691eb46a71858bd91
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-04-24 15:51:52 +0100

    VIA-2 MD/DB Restore mocking of content layer in vaccine page unit tests
    Re-introducing the concept of mocking allows testing of when errors are
    thrown in the content stack and de-couples the page unit tests from
    depending on the content stack being implemented correctly. A separate
    file, integration.test exists for testing that the real content stack
    and the individual vaccine pages work together correctly.

--- src/app/vaccines/6-in-1/page.test.tsx
+++ src/app/vaccines/6-in-1/page.test.tsx
@@ -1,34 +1,39 @@
-import { configProvider } from "@src/utils/config";
 import { render, screen } from "@testing-library/react";
 import Vaccine6in1 from "@src/app/vaccines/6-in-1/page";
 import Vaccine from "@src/app/_components/vaccine/Vaccine";
+import { getContentForVaccine } from "@src/services/content-api/gateway/content-reader-service";
+import { mockStyledContent } from "@test-data/content-api/data";
 
-jest.mock("@src/utils/config");
+jest.mock("@src/services/content-api/gateway/content-reader-service");
 jest.mock("@src/app/_components/vaccine/Vaccine", () => jest.fn(() => <div />));
 
 describe("6-in-1 vaccine page", () => {
-  (configProvider as jest.Mock).mockImplementation(() => ({
-    CONTENT_CACHE_PATH: "wiremock/__files/",
-    PINO_LOG_LEVEL: "info",
-  }));
+  describe("when content loaded successfully", () => {
+    beforeEach(() => {
+      (getContentForVaccine as jest.Mock).mockResolvedValue(
+        () => mockStyledContent,
+      );
+      (Vaccine as jest.Mock).mockImplementation(() => <div />);
+    });
 
-  it("should contain back link to vaccination schedule page", () => {
-    const pathToSchedulePage = "/schedule";
+    it("should contain back link to vaccination schedule page", () => {
+      const pathToSchedulePage = "/schedule";
 
-    render(Vaccine6in1());
+      render(Vaccine6in1());
 
-    const linkToSchedulePage = screen.getByRole("link", { name: "Go back" });
-    expect(linkToSchedulePage.getAttribute("href")).toBe(pathToSchedulePage);
-  });
+      const linkToSchedulePage = screen.getByRole("link", { name: "Go back" });
+      expect(linkToSchedulePage.getAttribute("href")).toBe(pathToSchedulePage);
+    });
 
-  it("should contain vaccine component", () => {
-    render(Vaccine6in1());
+    it("should contain vaccine component", () => {
+      render(Vaccine6in1());
 
-    expect(Vaccine).toHaveBeenCalledWith(
-      {
-        name: "6-in-1",
-      },
-      undefined,
-    );
+      expect(Vaccine).toHaveBeenCalledWith(
+        {
+          name: "6-in-1",
+        },
+        undefined,
+      );
+    });
   });
 });
--- src/app/vaccines/rsv/page.test.tsx
+++ src/app/vaccines/rsv/page.test.tsx
@@ -1,35 +1,40 @@
-import { configProvider } from "@src/utils/config";
 import { render, screen } from "@testing-library/react";
 import VaccineRsv from "@src/app/vaccines/rsv/page";
 import Vaccine from "@src/app/_components/vaccine/Vaccine";
+import { getContentForVaccine } from "@src/services/content-api/gateway/content-reader-service";
+import { mockStyledContent } from "@test-data/content-api/data";
 
-jest.mock("@src/utils/config");
+jest.mock("@src/services/content-api/gateway/content-reader-service");
 jest.mock("@src/app/_components/vaccine/Vaccine", () => jest.fn(() => <div />));
 
 describe("RSV vaccine page", () => {
-  (configProvider as jest.Mock).mockImplementation(() => ({
-    CONTENT_CACHE_PATH: "wiremock/__files/",
-    PINO_LOG_LEVEL: "info",
-  }));
+  describe("when content loaded successfully", () => {
+    beforeEach(() => {
+      (getContentForVaccine as jest.Mock).mockResolvedValue(
+        () => mockStyledContent,
+      );
+      (Vaccine as jest.Mock).mockImplementation(() => <div />);
+    });
 
-  it("should contain back link to vaccination schedule page", () => {
-    const pathToSchedulePage = "/schedule";
+    it("should contain back link to vaccination schedule page", () => {
+      const pathToSchedulePage = "/schedule";
 
-    render(VaccineRsv());
+      render(VaccineRsv());
 
-    const linkToSchedulePage = screen.getByRole("link", { name: "Go back" });
+      const linkToSchedulePage = screen.getByRole("link", { name: "Go back" });
 
-    expect(linkToSchedulePage.getAttribute("href")).toBe(pathToSchedulePage);
-  });
+      expect(linkToSchedulePage.getAttribute("href")).toBe(pathToSchedulePage);
+    });
 
-  it("should contain vaccine component", () => {
-    render(VaccineRsv());
+    it("should contain vaccine component", () => {
+      render(VaccineRsv());
 
-    expect(Vaccine).toHaveBeenCalledWith(
-      {
-        name: "RSV",
-      },
-      undefined,
-    );
+      expect(Vaccine).toHaveBeenCalledWith(
+        {
+          name: "RSV",
+        },
+        undefined,
+      );
+    });
   });
 });
