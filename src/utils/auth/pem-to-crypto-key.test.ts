d5ace18ffa2dee9b8715f84a774a32ab9c2366d5
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-28 18:54:31 +0100

    VIA-87 AJ/AS Modified pemToCryptoKey function to avoid two gets to the config

--- src/utils/auth/pem-to-crypto-key.test.ts
+++ src/utils/auth/pem-to-crypto-key.test.ts
@@ -3,10 +3,7 @@
  */
 
 import pemToCryptoKey from "@src/utils/auth/pem-to-crypto-key";
-import { configProvider } from "@src/utils/config";
 import { generateKeyPairSync } from "crypto";
-
-jest.mock("@src/utils/config");
 
 const generatePrivateKeyStub = (): string => {
   const { privateKey } = generateKeyPairSync("rsa", {
@@ -21,19 +18,11 @@ const generatePrivateKeyStub = (): string => {
 
 describe("pemToCryptoKey", () => {
   it("throws error for invalid key", () => {
-    (configProvider as jest.Mock).mockImplementation(() => ({
-      VACCINATION_APP_PRIVATE_KEY: "invalid-key",
-    }));
-
-    expect(pemToCryptoKey()).rejects.toThrow("Import key error");
+    expect(pemToCryptoKey("invalid-key")).rejects.toThrow("Import key error");
   });
 
   it("return CryptoKey for valid key", () => {
     const testPEMString: string = generatePrivateKeyStub();
-    (configProvider as jest.Mock).mockImplementation(() => ({
-      VACCINATION_APP_PRIVATE_KEY: testPEMString,
-    }));
-
-    expect(pemToCryptoKey()).resolves.toBeDefined();
+    expect(pemToCryptoKey(testPEMString)).resolves.toBeDefined();
   });
 });
