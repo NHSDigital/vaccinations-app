233c433909a5a4aa5e33e5b75d281d2d790d424b
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-04-28 13:33:49 +0100

    VIA-87 AJ/AS Added tests for middleware and handling sso within middleware

--- auth.ts
+++ auth.ts
@@ -1,6 +1,18 @@
 import NHSLoginAuthProvider from "@src/app/api/auth/[...nextauth]/provider";
 import NextAuth from "next-auth";
 
+const SSO_FAILURE_ROUTE = "/sso-failure";
+
 export const { handlers, signIn, signOut, auth } = NextAuth(async () => {
-  return { providers: [await NHSLoginAuthProvider()] };
+  return {
+    providers: [await NHSLoginAuthProvider()],
+    pages: {
+      signIn: SSO_FAILURE_ROUTE,
+      signOut: SSO_FAILURE_ROUTE,
+      error: SSO_FAILURE_ROUTE,
+      verifyRequest: SSO_FAILURE_ROUTE,
+      newUser: SSO_FAILURE_ROUTE
+    },
+    debug: process.env.NODE_ENV === "development"
+  };
 });
