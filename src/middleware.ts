20070792533ca284e698c7a910d241d43a206424
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-05-01 14:27:09 +0100

    VIA-87 AJ/AS Working sso routes with session checks in middleware. If the signIn is attempted in middleware, it leads to complex cookie merging issues eventually failing the state checks during the auth callback.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,16 @@
-import { auth, signIn } from "@project/auth";
-import { NHS_LOGIN_PROVIDER_ID } from "@src/app/api/auth/[...nextauth]/provider";
+import { auth } from "@project/auth";
 import { Session } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { logger } from "@src/utils/logger";
+import { Logger } from "pino";
 
-const log = logger.child({ name: "middleware" });
-const ASSERTED_LOGIN_IDENTITY_PARAM = "assertedLoginIdentity";
-
-const doSSO = async (request: NextRequest): Promise<NextResponse> => {
-  const assertedLoginIdentity: string | null = request.nextUrl.searchParams.get(
-    ASSERTED_LOGIN_IDENTITY_PARAM,
-  );
-
-  if (assertedLoginIdentity) {
-    const redirectUrl = await signIn(
-      NHS_LOGIN_PROVIDER_ID,
-      {
-        redirectUrl: request.url,
-        redirect: false,
-      },
-      { asserted_login_identity: assertedLoginIdentity },
-    );
-    log.info(`signIn wants to redirect to: ${redirectUrl}`);
-    return NextResponse.redirect(redirectUrl);
-  } else {
-    log.warn("SSO without assertedLoginIdentity parameter");
-    return NextResponse.redirect(
-      `${request.nextUrl.origin}/sso-failure?error=Parameter not found: ${ASSERTED_LOGIN_IDENTITY_PARAM}`,
-    );
-  }
-};
+const log: Logger = logger.child({ name: "middleware" });
 
 export async function middleware(request: NextRequest) {
   log.info(`Inspecting ${request.nextUrl}`);
   const session: Session | null = await auth();
-  if (!session?.expires) {
-    try {
-      return await doSSO(request);
-    } catch (error) {
-      log.error(error);
-      return NextResponse.redirect(
-        `${request.nextUrl.origin}/sso-failure?error=${error}`,
-      );
-    }
+  if (!session?.user?.birthdate) {
+    return NextResponse.redirect(new URL(`/sso-failure?error=No active session found`, request.url));
   }
   log.info(session, "Session Object");
   return NextResponse.next();
@@ -53,11 +21,11 @@ export const config = {
     /*
      * Apply middleware to all pages except:
      * 1. /api/auth (exclude all NextAuth routes)
-     * 2. /sso (exclude the SSO routes)
+     * 2. /sso-failure (exclude the SSO routes)
      * 3. /_next/* (exclude Next.js assets, e.g., /_next/static/*)
      * 4. /favicon.ico (exclude the favicon path)
      * 5. /nhsuk-frontend-9.1.0 (exclude nhsuk js)
      */
-    "/((?!api/auth|sso-failure|favicon.ico|nhsuk-frontend-9.1.0|_next/static|_next/image).*)",
-  ],
+    "/((?!api/auth|api/sso|sso-failure|favicon.ico|nhsuk-frontend-9.1.0|_next/static|_next/image).*)"
+  ]
 };
