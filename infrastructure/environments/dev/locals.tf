3d8b9aeb8932559397b5439d4bfada941aa96bbf
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-05-01 18:16:49 +0100

    VIA-87 AJ Moved cache hydrator as a warmer to its own module to decouple the concerns

--- infrastructure/environments/dev/locals.tf
+++ infrastructure/environments/dev/locals.tf
@@ -6,22 +6,38 @@ locals {
 
   environment               = "dev"
   git_branch                = coalesce(data.external.git_branch.result.output, "na")
-  deploy_workspace          = terraform.workspace == "default" ? "gh" : terraform.workspace
+  deploy_workspace          = var.is_github_action ? "gh" : terraform.workspace
   prefix                    = "${local.deploy_workspace}-${local.git_branch}-${local.project_identifier_shortcode}-${data.aws_caller_identity.current.account_id}"
   open_next_path            = "../../../.open-next"
   node_version              = "nodejs22.x"
   log_retention_in_days     = 7
   cache_lambda_zip_path     = "../../../lambda.zip"
-  ssm_prefix                = "/${local.prefix}/"
-  pino_log_level            = "info"
-  content_api_endpoint      = "https://sandbox.api.service.nhs.uk/"
   content_cache_bucket_name = "${local.prefix}-content-cache"
-  nhs_login_url             = "https://auth.sandpit.signin.nhs.uk"
-  nhs_login_scope           = "openid profile gp_registration_details"
+
+  application_environment_variables = {
+    SSM_PREFIX           = "/${local.prefix}/"
+
+    PINO_LOG_LEVEL       = "info"
+
+    CONTENT_API_ENDPOINT = "https://sandbox.api.service.nhs.uk/"
+    CONTENT_CACHE_PATH   = "s3://${local.content_cache_bucket_name}"
+
+    NHS_LOGIN_URL        = "https://auth.sandpit.signin.nhs.uk"
+    NHS_LOGIN_SCOPE      = "openid profile gp_registration_details"
+
+    AUTH_TRUST_HOST      = "true"
+    AUTH_SECRET          = random_password.auth_secret.result
+  }
 
   default_tags = {
     ManagedBy   = "Terraform"
     Project     = local.project_identifier
     Environment = local.environment
   }
+}
+
+resource "random_password" "auth_secret" {
+  length           = 64
+  special          = true
+  override_special = "/+"
 }
--- infrastructure/modules/deploy_app/iam.tf
+++ infrastructure/modules/deploy_app/iam.tf
@@ -1,9 +1,4 @@
 resource "aws_iam_policy" "server_lambda_additional_policy" {
   name   = "${var.prefix}-server-lambda-additional-policy"
-  policy = templatefile("${path.module}/iam_policies/server-lambda-iam-policy.json", {})
-}
-
-resource "aws_iam_policy" "cache_lambda_additional_policy" {
-  name   = "${var.prefix}-cache-lambda-additional-policy"
-  policy = templatefile("${path.module}/iam_policies/cache-lambda-iam-role-policy.json", {})
+  policy = templatefile("${path.module}/policies/server-lambda-iam-policy.json", {})
 }
