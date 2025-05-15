4950c58088f952ecf31ef539db0ed9e3adb2604c
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-05-15 12:24:37 +0100

    VIA-171 AJ/DB Added MainContent component as it is now moved from layout to individual pages

---
+++ src/app/_components/nhs-frontend/MainContent.tsx
@@ -0,0 +1,20 @@
+import React, { JSX } from "react";
+
+const MainContent = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>): JSX.Element => {
+  return (
+    <main
+      className="nhsuk-main-wrapper nhsuk-main-wrapper--s"
+      id="maincontent"
+    >
+      <div className="nhsuk-grid-row">
+        <div className="nhsuk-grid-column-full">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default MainContent;
