b63f877b319e529413a58f2a5fbee5d144426c7a
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-05-29 12:17:11 +0100

    VIA-172 AJ Comments for the types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,9 +1,12 @@
 interface Window {
+  // Reference: https://nhsconnect.github.io/nhsapp-developer-documentation/js-v2-api-specification/
   nhsapp: {
+    // tools functions are available within both native mobile and desktop app
     tools: {
       getAppPlatform: () => string;
       isOpenInNHSApp: () => boolean;
     },
+    // navigation functions are only available within native mobile app
     navigation: {
       goToHomePage: () => void;
       openBrowserOverlay: (overlayUri: string) => void;
