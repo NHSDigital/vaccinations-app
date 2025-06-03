2b23bb038d11706b6dc1d08cddc03f67cdc6587f
James Salt <205939362+james-salt4-nhs@users.noreply.github.com>
2025-06-03 11:41:18 +0100

    VIA-227 JS Move to 5 runs averaged, log the individual values and remove dead code

--- e2e/e2e-helpers.ts
+++ e2e/e2e-helpers.ts
@@ -5,25 +5,22 @@ export const clickLinkAndExpectPageTitle = async (page: Page,  linkText: string,
   await expect(page).toHaveTitle(expectedPageTitle);
 }
 
-export const determineLargestContentfulPaint = async () => {
-
-}
-
-const lcpTime = async (): Promise<number> => {
+const lcpDuration = async (): Promise<number> => {
   return new Promise((resolve) => {
     new PerformanceObserver((entryList) => {
       const entries = entryList.getEntries();
-      const lastEntry = entries[entries.length - 1];
-      resolve(lastEntry.duration);
+      const lcpDuration = entries[entries.length -1].duration;
+      console.log("LCP: " + lcpDuration);
+      resolve(lcpDuration);
     }).observe({type: 'largest-contentful-paint', buffered: true});
   });
 }
 
 export const benchmark = async (page: Page, target: string) => {
   const pageLoadTimes: number[] = [];
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 5; i++) {
     await page.goto(target, { waitUntil: 'load' });
-    pageLoadTimes.push(await page.evaluate(lcpTime));
+    pageLoadTimes.push(await page.evaluate(lcpDuration));
   }
   const sumPageLoadTimes = pageLoadTimes.reduce((sum, cur) => sum + cur, 0);
   console.log(sumPageLoadTimes);
