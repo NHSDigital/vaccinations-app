57ef30aa9a5203d31442cd48401549ae8c7b827a
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-06-03 16:46:04 +0100

    VIA-172 AJ Formatting

--- src/app/session-timeout/page.tsx
+++ src/app/session-timeout/page.tsx
@@ -15,10 +15,12 @@ const SessionTimeout = () => {
       <title>You have been logged out</title>
       <h1>You have been logged out</h1>
       <p>
-        For security reasons, you&#39;re automatically logged out if you have not used the service for 10 minutes.
+        For security reasons, you&#39;re automatically logged out if you have
+        not used the service for 10 minutes.
       </p>
       <p>
-        If you were entering information, it has not been saved and you will need to re-enter it.
+        If you were entering information, it has not been saved and you will
+        need to re-enter it.
       </p>
       <p>
         To continue, close the tab which has the service open and login again.
