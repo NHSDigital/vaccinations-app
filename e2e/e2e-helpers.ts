b17d3dbb1ed630aec9ef7af692a7e46bf946fb85
James Salt <205939362+james-salt4-nhs@users.noreply.github.com>
2025-06-03 11:30:19 +0100

    VIA-227 JS Implement LCP rather than full page load as benchmark timing

--- e2e/e2e-helpers.ts
+++ e2e/e2e-helpers.ts
@@ -5,14 +5,25 @@ export const clickLinkAndExpectPageTitle = async (page: Page,  linkText: string,
   await expect(page).toHaveTitle(expectedPageTitle);
 }
 
+export const determineLargestContentfulPaint = async () => {
+
+}
+
+const lcpTime = async (): Promise<number> => {
+  return new Promise((resolve) => {
+    new PerformanceObserver((entryList) => {
+      const entries = entryList.getEntries();
+      const lastEntry = entries[entries.length - 1];
+      resolve(lastEntry.duration);
+    }).observe({type: 'largest-contentful-paint', buffered: true});
+  });
+}
+
 export const benchmark = async (page: Page, target: string) => {
-  const pageLoadTimes = [];
-  let start;
+  const pageLoadTimes: number[] = [];
   for (let i = 0; i < 3; i++) {
-    start = performance.now();
-    await page.goto(target);
-    await page.waitForLoadState('load');
-    pageLoadTimes.push(performance.now() - start);
+    await page.goto(target, { waitUntil: 'load' });
+    pageLoadTimes.push(await page.evaluate(lcpTime));
   }
   const sumPageLoadTimes = pageLoadTimes.reduce((sum, cur) => sum + cur, 0);
   console.log(sumPageLoadTimes);
