10650265e9c693b059bd872c4651e2984faef004
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-06-04 16:55:39 +0100

    VIA-183 AS/DB Fix setting of session in callback function

--- src/utils/auth/callbacks/get-updated-session.ts
+++ src/utils/auth/callbacks/get-updated-session.ts
@@ -2,11 +2,15 @@ import { Session } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
 const getUpdatedSession = (session: Session, token: JWT) => {
+  console.log(token);
+  console.log(session);
   if (token?.user && session.user) {
     session.user.nhs_number = token.user.nhs_number;
     session.user.birthdate = token.user.birthdate;
     session.user.access_token = token.access_token;
-    session.user.id_token.jti = token.id_token.jti;
+    session.user.id_token = {
+      jti: token.id_token.jti,
+    };
   }
 
   return session;
