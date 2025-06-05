30f5e7d464bbc8878687b03212a9b6937893e3c1
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-06-05 17:56:49 +0100

    AJ Removing the hard coded URL path

--- src/app/api/sso/route.ts
+++ src/app/api/sso/route.ts
@@ -1,5 +1,6 @@
 import { signIn } from "@project/auth";
 import { NHS_LOGIN_PROVIDER_ID } from "@src/app/api/auth/[...nextauth]/provider";
+import { SSO_FAILURE_ROUTE } from "@src/app/sso-failure/constants";
 import { logger } from "@src/utils/logger";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
@@ -25,11 +26,11 @@ export const GET = async (request: NextRequest) => {
       errorString = `${error}`;
       log.error(error);
     }
-    redirect(redirectUrl ?? `/sso-failure?error=${errorString}`);
+    redirect(redirectUrl ?? `${SSO_FAILURE_ROUTE}?error=${errorString}`);
   } else {
     log.warn("SSO without assertedLoginIdentity parameter");
     redirect(
-      `/sso-failure?error=Parameter not found: ${ASSERTED_LOGIN_IDENTITY_PARAM}`,
+      `${SSO_FAILURE_ROUTE}?error=Parameter not found: ${ASSERTED_LOGIN_IDENTITY_PARAM}`,
     );
   }
 };
