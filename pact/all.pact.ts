0a686b5dfaa7ecef3a623644d7823a0285782a39
James Salt <205939362+james-salt4-nhs@users.noreply.github.com>
2025-06-05 10:15:54 +0100

    VIA-226 JS Check there are no errors from the api call

--- pact/all.pact.ts
+++ pact/all.pact.ts
@@ -5,6 +5,7 @@ import { VaccineTypes } from "@src/models/vaccine";
 describe('Content API', () => {
   test("getContentForVaccine contract", async () => {
     const res = await getContentForVaccine(VaccineTypes.RSV);
+    expect(res.contentError).toBeNull();
     console.log(res.styledVaccineContent);
     expect(res.styledVaccineContent).toEqual({});
   });
