762ab8c4a127f2405bde43e1ad79a26708b48cdc
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-06-05 18:06:41 +0100

    VIA-183 AS/DB Refactor: extract signed JWT method from generateClientAssertion

--- src/utils/auth/generate-refresh-client-assertion.ts
+++ src/utils/auth/generate-refresh-client-assertion.ts
@@ -1,8 +1,7 @@
 import { AppConfig } from "@src/utils/config";
-import pemToCryptoKey from "@src/utils/auth/pem-to-crypto-key";
+import { generateSignedJwt } from "@src/utils/auth/generate-signed-jwt";
 
 const generateClientAssertion = async (config: AppConfig): Promise<string> => {
-  const privateKey = await pemToCryptoKey(config.NHS_LOGIN_PRIVATE_KEY);
   const now = Math.floor(Date.now() / 1000);
   const payload = {
     iss: config.NHS_LOGIN_CLIENT_ID,
@@ -13,23 +12,7 @@ const generateClientAssertion = async (config: AppConfig): Promise<string> => {
     iat: now,
   };
 
-  const header = { alg: "RS512", typ: "JWT" };
-  const encodedHeader = Buffer.from(JSON.stringify(header)).toString(
-    "base64url",
-  );
-  const encodedPayload = Buffer.from(JSON.stringify(payload)).toString(
-    "base64url",
-  );
-
-  const signatureInput = `${encodedHeader}.${encodedPayload}`;
-  const signature = await crypto.subtle.sign(
-    { name: "RSASSA-PKCS1-v1_5", hash: "SHA-512" },
-    privateKey,
-    new TextEncoder().encode(signatureInput),
-  );
-
-  const encodedSignature = Buffer.from(signature).toString("base64url");
-  return `${signatureInput}.${encodedSignature}`;
+  return await generateSignedJwt(config, payload);
 };
 
 export { generateClientAssertion };
