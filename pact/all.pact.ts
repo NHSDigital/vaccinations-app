ddecf4d56441bbd56a7cc6ae58980fc18afbd541
James Salt <205939362+james-salt4-nhs@users.noreply.github.com>
2025-06-06 12:26:24 +0100

    VIA-226 JS Add simple comparison of cache with api call

--- pact/all.pact.ts
+++ pact/all.pact.ts
@@ -1,8 +1,24 @@
 import { readFile } from "node:fs/promises";
+import axios, { AxiosResponse } from "axios";
+import dotenv from 'dotenv';
 
 describe('Content API', () => {
-  test("getContentForVaccine contract", async () => {
-    const vaccineContent = await readFile("wiremock/__files/rsv-vaccine.json", { encoding: "utf8" });
-    console.log(JSON.parse(vaccineContent));
+  beforeAll(async () => {
+    dotenv.config({path: ".env.local"});  // Load environment variables from .env file
+  });
+
+  test("vaccineContent contract", async () => {
+    const expected = JSON.parse(await readFile("wiremock/__files/rsv-vaccine.json", { encoding: "utf8" }));
+
+    const actualEndpoint = process.env["CONTENT_API_ENDPOINT"] + "/nhs-website-content/vaccinations/rsv-vaccine";
+    const response: AxiosResponse = await axios.get(actualEndpoint,
+      {
+        headers: {
+          accept: "application/json",
+          apikey: process.env["CONTENT_API_KEY"]
+        }
+      });
+    const actual = response.data;
+    expect(actual).toEqual(expected);
   });
 });
