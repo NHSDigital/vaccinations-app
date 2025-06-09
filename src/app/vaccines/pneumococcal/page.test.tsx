4e76b9c60211ca01d12a61713d7d0ee27d630c46
Marie Dedikova <197628467+marie-dedikova-nhs@users.noreply.github.com>
2025-06-09 10:49:42 +0100

    VIA-206 MD Add mocked eligibility call to each vaccine page

--- src/app/vaccines/pneumococcal/page.test.tsx
+++ src/app/vaccines/pneumococcal/page.test.tsx
@@ -5,8 +5,12 @@ import { mockStyledContent } from "@test-data/content-api/data";
 import { VaccineTypes } from "@src/models/vaccine";
 import VaccinePneumococcal from "@src/app/vaccines/pneumococcal/page";
 import { assertBackLinkIsPresent } from "@test-data/test-helpers-back-link";
+import { getEligibilityForPerson } from "@src/services/eligibility-api/gateway/eligibility-reader-service";
+import { EligibilityStatus } from "@src/services/eligibility-api/types";
+import { mockStyledEligibility } from "@test-data/eligibility-api/data";
 
 jest.mock("@src/services/content-api/gateway/content-reader-service");
+jest.mock("@src/services/eligibility-api/gateway/eligibility-reader-service");
 jest.mock("@src/app/_components/vaccine/Vaccine");
 jest.mock("@src/app/_components/nhs-frontend/BackLink", () =>
   jest.fn(() => <div data-testid="back-link"></div>),
@@ -17,6 +21,10 @@ describe("Pneumococcal vaccine page", () => {
     beforeEach(() => {
       (getContentForVaccine as jest.Mock).mockResolvedValue({
         styledVaccineContent: mockStyledContent,
+      });
+      (getEligibilityForPerson as jest.Mock).mockResolvedValue({
+        eligibilityStatus: EligibilityStatus.ELIGIBLE_BOOKABLE,
+        styledEligibilityContent: mockStyledEligibility,
       });
       (Vaccine as jest.Mock).mockImplementation(() => <div />);
     });
