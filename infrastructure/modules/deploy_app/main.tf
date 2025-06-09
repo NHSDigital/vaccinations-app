c321f220c66fd1c92c5df86ee397902c7304fa07
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-06-09 09:24:42 +0100

    AJ Upgrading deploy_app module

--- infrastructure/modules/deploy_app/main.tf
+++ infrastructure/modules/deploy_app/main.tf
@@ -1,7 +1,7 @@
 module "deploy_app" {
   source            = "RJPearson94/open-next/aws//modules/tf-aws-open-next-zone"
-  version           = ">= 3.4.0,< 3.5.0"
-  open_next_version = "v3.5.3"
+  version           = "3.6.0"
+  open_next_version = "v3.x.x"
 
   providers = {
     aws.server_function = aws.server_function
@@ -22,6 +22,10 @@ module "deploy_app" {
     viewer_certificate = {
       acm_certificate_arn = var.acm_certificate_arn
     }
+  }
+
+  continuous_deployment = {
+    use = false
   }
 
   server_function = {
