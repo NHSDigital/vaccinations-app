eb102e6b9d1e126fcfee10aa9adbf2e5d4a75c08
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-06-11 16:59:46 +0100

    VIA-226 Match Vaccine Contract to previous state allowing for changes in lastReviewed field

--- pact/all.pact.ts
+++ pact/all.pact.ts
@@ -2,14 +2,15 @@ import axios, { AxiosResponse } from "axios";
 import dotenv from 'dotenv';
 import { readFile } from "node:fs/promises";
 
-describe('Content API', () => {
+describe('Content API contract', () => {
   beforeAll(async () => {
     dotenv.config({path: ".env.local"});
   });
 
-  test("vaccineContent contract", async () => {
+  it("should match RSV Vaccine Content tolerating changes in LastReviewed dates", async () => {
     const actualEndpoint = process.env["CONTENT_API_ENDPOINT"] + "/nhs-website-content/vaccinations/rsv-vaccine";
     const expected = JSON.parse(await readFile("wiremock/__files/rsv-vaccine.json", { encoding: "utf8" }));
+
     const response: AxiosResponse = await axios.get(actualEndpoint,
       {
         headers: {
@@ -17,6 +18,16 @@ describe('Content API', () => {
           apikey: process.env["CONTENT_API_KEY"]
         }
       });
-    expect(response.data).toEqual(expected);
+
+    expectResponseMatchesWithAnyValueForLastReviewed(response.data, expected);
   });
+
+  const expectResponseMatchesWithAnyValueForLastReviewed = (actualVaccineData: any, expectedVaccineData: any) => {
+    const {lastReviewed: expectedLastReviewed, ...expectedVaccineContent} = expectedVaccineData;
+    const {lastReviewed: actualLastReviewed, ...actualVaccineContent} = actualVaccineData;
+
+    expect(actualVaccineContent).toEqual(expectedVaccineContent);
+    expect(actualLastReviewed).toEqual(expect.any(Array)); // replace with line below after jest30
+    // expect(actualLastReviewed).toEqual(expect.arrayOf(expect.any(String))); // New arrayOf assertion will be available in Jest v30
+  };
 });
