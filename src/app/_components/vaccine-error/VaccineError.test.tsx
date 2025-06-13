4c163ebd899ac2f1e05a69e0019be1681b51b2d9
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-06-13 18:13:29 +0100

    VIA-241 AJ Removed heading from vaccine error to be moved up the component hierarchy, so that it can be reused on hub and vaccine page

--- src/app/_components/vaccine-error/VaccineError.test.tsx
+++ src/app/_components/vaccine-error/VaccineError.test.tsx
@@ -1,64 +1,38 @@
 import { render, screen } from "@testing-library/react";
-import { VaccineInfo, VaccineTypes } from "@src/models/vaccine";
 import VaccineError from "@src/app/_components/vaccine-error/VaccineError";
 
 describe("VaccineError", () => {
-  const allVaccineTypes = Object.values(VaccineTypes) as VaccineTypes[];
+  it("should display subheading", async () => {
+    render(<VaccineError />);
 
-  it.each(allVaccineTypes)(
-    `should display vaccine name in error heading for %s`,
-    (vaccineType: VaccineTypes) => {
-      render(VaccineError({ vaccineType: vaccineType }));
+    const subheading: HTMLElement = screen.getByRole("heading", {
+      level: 2,
+      name: "Vaccine content is unavailable",
+    });
 
-      const heading: HTMLElement = screen.getByRole("heading", {
-        level: 1,
-        name: `${VaccineInfo[vaccineType].displayName.capitalised} vaccine`,
-      });
+    expect(subheading).toBeInTheDocument();
+  });
 
-      expect(heading).toBeInTheDocument();
-    },
-  );
+  it("should display error text", async () => {
+    render(<VaccineError />);
 
-  it.each(allVaccineTypes)(
-    "should display subheading for %s",
-    (vaccineType: VaccineTypes) => {
-      render(VaccineError({ vaccineType: vaccineType }));
+    const text: HTMLElement = screen.getByText(
+      /Sorry, there is a problem showing vaccine/,
+    );
 
-      const subheading: HTMLElement = screen.getByRole("heading", {
-        level: 2,
-        name: "Vaccine content is unavailable",
-      });
+    expect(text).toBeInTheDocument();
+  });
 
-      expect(subheading).toBeInTheDocument();
-    },
-  );
+  it("renders link in error text", async () => {
+    render(<VaccineError />);
 
-  it.each(allVaccineTypes)(
-    "should display error text for %s",
-    (vaccineType: VaccineTypes) => {
-      render(VaccineError({ vaccineType: vaccineType }));
+    const link: HTMLElement = screen.getByRole("link", {
+      name: "vaccinations on NHS.uk",
+    });
 
-      const text: HTMLElement = screen.getByText(
-        /Sorry, there is a problem showing vaccine/,
-      );
-
-      expect(text).toBeInTheDocument();
-    },
-  );
-
-  it.each(allVaccineTypes)(
-    "renders link in error text for %s",
-    async (vaccineType: VaccineTypes) => {
-      render(VaccineError({ vaccineType: vaccineType }));
-
-      const link: HTMLElement = screen.getByRole("link", {
-        name: "vaccinations on NHS.uk",
-      });
-
-      expect(link).toBeInTheDocument();
-      expect(link.getAttribute("href")).toEqual(
-        "https://www.nhs.uk/vaccinations/",
-      );
-    },
-  );
+    expect(link).toBeInTheDocument();
+    expect(link.getAttribute("href")).toEqual(
+      "https://www.nhs.uk/vaccinations/",
+    );
+  });
 });
--- src/app/_components/vaccine-error/VaccineError.tsx
+++ src/app/_components/vaccine-error/VaccineError.tsx
@@ -1,36 +1,26 @@
 "use client";
 
 import { JSX } from "react";
-import { VaccineInfo, VaccineTypes } from "@src/models/vaccine";
 import { ErrorSummary } from "nhsuk-react-components";
 
-interface VaccineProps {
-  vaccineType: VaccineTypes;
-}
-
-const VaccineError = ({ vaccineType }: VaccineProps): JSX.Element => {
-  const vaccineInfo = VaccineInfo[vaccineType];
-
+const VaccineError = (): JSX.Element => {
   return (
-    <div>
-      <h1 className="app-dynamic-page-title__heading">{`${vaccineInfo.displayName.capitalised} vaccine`}</h1>
-      <ErrorSummary>
-        <ErrorSummary.Title>Vaccine content is unavailable</ErrorSummary.Title>
-        <ErrorSummary.Body>
-          <p>
-            Sorry, there is a problem showing vaccine information. Come back
-            later, or read about{" "}
-            <a
-              href="https://www.nhs.uk/vaccinations/"
-              target="_blank"
-              rel="noopener"
-            >
-              vaccinations on NHS.uk
-            </a>
-          </p>
-        </ErrorSummary.Body>
-      </ErrorSummary>
-    </div>
+    <ErrorSummary>
+      <ErrorSummary.Title>Vaccine content is unavailable</ErrorSummary.Title>
+      <ErrorSummary.Body>
+        <p>
+          Sorry, there is a problem showing vaccine information. Come back
+          later, or read about{" "}
+          <a
+            href="https://www.nhs.uk/vaccinations/"
+            target="_blank"
+            rel="noopener"
+          >
+            vaccinations on NHS.uk
+          </a>
+        </p>
+      </ErrorSummary.Body>
+    </ErrorSummary>
   );
 };
 
