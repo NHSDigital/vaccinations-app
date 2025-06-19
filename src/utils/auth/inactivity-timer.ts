81b99c562f9aaa84386e443e7ced84552645ac1c
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-06-19 14:51:42 +0100

    VIA-89 AJ/AS Removed touchstart event as that was leading to duplicate events as touch translates to click event eventually. Also, keyup is less intrusive when someone keeps pressing a key

--- src/utils/auth/inactivity-timer.ts
+++ src/utils/auth/inactivity-timer.ts
@@ -5,10 +5,9 @@ import { useCallback, useEffect, useRef, useState } from "react";
 export const WARNING_TIME_MS: number = 9 * 60 * 1000;
 const LOGOUT_TIME_MS: number = 10 * 60 * 1000;
 export const ACTIVITY_EVENTS: string[] = [
-  "keydown",
+  "keyup",
   "click",
   "scroll",
-  "touchstart",
 ];
 
 const useInactivityTimer = (
