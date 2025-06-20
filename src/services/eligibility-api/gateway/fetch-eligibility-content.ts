fb79da3bcc25f6f846cf4f9e4de484650e2da20e
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-06-20 10:43:14 +0100

    VIA-264 Change EliD tracing to use X-Request-ID field
    As requested by EliD in cross-team discussions

--- src/services/eligibility-api/gateway/fetch-eligibility-content.ts
+++ src/services/eligibility-api/gateway/fetch-eligibility-content.ts
@@ -14,7 +14,7 @@ export const fetchEligibilityContent = async (
 
   const apiEndpoint: string = config.ELIGIBILITY_API_ENDPOINT;
   const apiKey: string = config.ELIGIBILITY_API_KEY;
-  const correlationId: string | undefined = process.env._X_AMZN_TRACE_ID;
+  const vitaTraceId: string | undefined = process.env._X_AMZN_TRACE_ID;
 
   const uri: string = `${apiEndpoint}${ELIGIBILITY_API_PATH_SUFFIX}${nhsNumber}`;
   let response: AxiosResponse;
@@ -24,7 +24,7 @@ export const fetchEligibilityContent = async (
       headers: {
         accept: "application/json",
         apikey: apiKey,
-        "X-Correlation-ID": correlationId,
+        "X-Request-ID": vitaTraceId,
       },
     });
     log.info("Successfully fetched content from %s", uri);
