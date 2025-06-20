2cc9b4f0159a16a791b203819cc6b731fd8ea2f2
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-06-20 16:59:49 +0100

    VIA-264 AJ/DB Extract RootID from amznTraceId, refactor shared code in logger
    Amazon documentation describes the amznTraceId field as chaning several
    properties together, including the traceId of the current place in the
    execution. Extract this rootId as it is more readable than the whole
    string.
    
    Also extracts logic common to the loggers for both the NextJS Middleware
    Edge Runtime and Node into a single shared location.

---
+++ src/utils/logger.test.ts
@@ -0,0 +1,20 @@
+import { extractRootTraceIdFromAmznTraceId } from "@src/utils/logger";
+
+describe("extractRootTraceIdFromAmznTraceId", () => {
+  it("should extract TraceID root from longer AWS Trace Id string", () => {
+    const amznTraceId =
+      "Root=1-68557034-317ef01c7e77ec2d5f31e4bb;Parent=28e363d17cb3d67b;Sampled=0;Lineage=1:a14f8d3b:0";
+
+    const traceIdRoot = extractRootTraceIdFromAmznTraceId(amznTraceId);
+
+    expect(traceIdRoot).toEqual("1-68557034-317ef01c7e77ec2d5f31e4bb");
+  });
+
+  it("should return undefined if root not found in full traceId string", () => {
+    const invalidAmznTraceId = "not-the-correct-format";
+
+    const traceIdRoot = extractRootTraceIdFromAmznTraceId(invalidAmznTraceId);
+
+    expect(traceIdRoot).toBeUndefined();
+  });
+});
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,20 +3,33 @@ import { asyncLocalStorage } from "@src/utils/requestContext";
 
 const isEdgeRuntime = process?.env?.NEXT_RUNTIME === "edge";
 
+const formatterWithLevelAsText = {
+  level: (label: string) => {
+    return { level: label.toUpperCase() };
+  },
+};
+
+export const extractRootTraceIdFromAmznTraceId = (amznTraceId: string) => {
+  const amznTraceIdRootExtractionRegex = /Root=([\d\-\w]*);.*/;
+  return amznTraceId.match(amznTraceIdRootExtractionRegex)?.[1];
+};
+
+const applicationContextFields = {
+  traceId: process.env._X_AMZN_TRACE_ID
+    ? extractRootTraceIdFromAmznTraceId(process.env._X_AMZN_TRACE_ID)
+    : undefined,
+  lambdaVersion: process.env.AWS_LAMBDA_FUNCTION_VERSION,
+  appVersion: process.env.APP_VERSION,
+};
+
 const pinoLoggerForNode = () => {
   return pino({
     level: process.env.PINO_LOG_LEVEL ?? "info",
-    formatters: {
-      level: (label) => {
-        return { level: label.toUpperCase() };
-      },
-    },
+    formatters: formatterWithLevelAsText,
     mixin() {
       return {
         requestId: asyncLocalStorage?.getStore()?.requestId,
-        traceId: process.env._X_AMZN_TRACE_ID,
-        lambdaVersion: process.env.AWS_LAMBDA_FUNCTION_VERSION,
-        appVersion: process.env.APP_VERSION,
+        ...applicationContextFields,
       };
     },
   });
@@ -25,16 +38,10 @@ const pinoLoggerForNode = () => {
 const pinoLoggerForEdge = () => {
   return pino({
     browser: {
+      formatters: formatterWithLevelAsText,
       write: (logEvent: LogDescriptor) => {
-        logEvent.traceId = process.env._X_AMZN_TRACE_ID;
-        logEvent.lambdaVersion = process.env.AWS_LAMBDA_FUNCTION_VERSION;
-        logEvent.appVersion = process.env.APP_VERSION;
+        logEvent = { ...logEvent, ...applicationContextFields };
         console.log(JSON.stringify(logEvent));
-      },
-      formatters: {
-        level: (label) => {
-          return { level: label.toUpperCase() };
-        },
       },
     },
   });
