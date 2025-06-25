29bc74b0575fbcfe6bc81a6c001e4f4f0ea13f6d
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-06-25 18:40:52 +0100

    VIA-289 AJ/DB Fixing the AWS module version to major.minor and allow patch to be upgraded

--- infrastructure/environments/preprod/s3.tf
+++ infrastructure/environments/preprod/s3.tf
@@ -1,6 +1,6 @@
 module "content_cache_s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.11.0"
+  version = "~>4.11.0"
 
   bucket        = local.content_cache_bucket_name
   force_destroy = true
