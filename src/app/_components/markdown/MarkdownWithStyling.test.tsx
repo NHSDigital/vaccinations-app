6c34b2bea504f9926d02b011cb0985c12bc4b02c
Marie Dedikova <197628467+marie-dedikova-nhs@users.noreply.github.com>
2025-06-26 16:10:45 +0100

    VIA-325 MD/AS: Improved a test for MarkdownWithStyling component being called with correct content & refactored MarkdownWithStyling component

--- src/app/_components/markdown/MarkdownWithStyling.test.tsx
+++ src/app/_components/markdown/MarkdownWithStyling.test.tsx
@@ -5,22 +5,38 @@ import {
 } from "@src/app/_components/markdown/MarkdownWithStyling";
 import React from "react";
 
-jest.mock("react-markdown", () => {
-  return function MockMarkdown({ children }: { children: React.ReactNode }) {
-    return <div>{children}</div>;
-  };
-});
+let mockedMarkdown: jest.Mock;
+
+jest.mock("react-markdown", () => ({
+  __esModule: true,
+  default: (...args: []) => mockedMarkdown(...args),
+}));
 
 describe("markdown", () => {
   describe("MarkdownWithStyling", () => {
-    it("should display markdown content", () => {
-      const content = "This is content";
-      render(MarkdownWithStyling({ content }));
-
-      const displayedContent: HTMLElement = screen.getByText(content);
-
-      expect(displayedContent).toBeVisible();
+    beforeEach(() => {
+      mockedMarkdown = jest.fn(({ children }) => <div>{children}</div>);
     });
+    it("should call markdown component with correct content", () => {
+      const content = "This is content";
+      render(<MarkdownWithStyling content={content} />);
+
+      expect(mockedMarkdown).toHaveBeenCalledWith(
+        expect.objectContaining({ children: content }),
+        undefined,
+      );
+    });
+
+    // it("should call markdown component with correct content 2", () => {
+    //   mockedMarkdown = jest.fn(() => undefined);
+    //   const content = "This is content";
+    //   render(<MarkdownWithStyling content={content} />);
+    //
+    //   expect(mockedMarkdown).toHaveBeenCalledWith(
+    //     expect.objectContaining({ children: undefined }),
+    //     undefined
+    //   );
+    // });
   });
 
   describe("H2", () => {
--- src/app/_components/markdown/MarkdownWithStyling.tsx
+++ src/app/_components/markdown/MarkdownWithStyling.tsx
@@ -1,21 +1,20 @@
 import Markdown from "react-markdown";
-import { ReactNode } from "react";
+import { HTMLAttributes, ReactNode } from "react";
 import { JSX } from "react/jsx-runtime";
-import IntrinsicAttributes = JSX.IntrinsicAttributes;
 
 interface MarkdownProps {
   content: string;
 }
 
-interface H2Props {
-  children: IntrinsicAttributes & ReactNode;
+interface H2Props extends HTMLAttributes<HTMLHeadingElement> {
+  children?: ReactNode;
 }
 
 const MarkdownWithStyling = ({ content }: MarkdownProps): JSX.Element => {
   return (
     <Markdown
       components={{
-        h2: ({ children }) => (children ? <H2>{children}</H2> : undefined),
+        h2: H2,
       }}
     >
       {content}
@@ -23,8 +22,12 @@ const MarkdownWithStyling = ({ content }: MarkdownProps): JSX.Element => {
   );
 };
 
-const H2 = ({ children }: H2Props): JSX.Element => {
-  return <h2 className="nhsuk-heading-s">{children}</h2>;
+const H2 = ({ children, ...props }: H2Props): JSX.Element => {
+  return (
+    <h2 className="nhsuk-heading-s" {...props}>
+      {children}
+    </h2>
+  );
 };
 
 export { MarkdownWithStyling, H2 };
