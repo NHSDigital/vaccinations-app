759eebe462aaee5fc152efff911633d48311d09f
Simon Brunning <196635190+acerathereinspirative-nhs-nvir@users.noreply.github.com>
2025-06-26 09:26:26 +0100

    VIA-289 SB Add contract test cases.

--- contract/eligibility-api.contract.ts
+++ contract/eligibility-api.contract.ts
@@ -9,11 +9,29 @@ describe("EliD API contract", () => {
   });
 
   describe("Actionable", () => {
-    it("should be actionable...", async () => {
+    it("should be actionable ", async () => {
       const eligibilityForPerson = await getEligibilityForPerson(VaccineTypes.RSV, "9686368973");
 
       expect(Object.keys(eligibilityForPerson).length).toEqual(2);
       expect(eligibilityForPerson.eligibility.status).toEqual(EligibilityStatus.ACTIONABLE);
+    });
+  });
+
+  describe("Not Actionable", () => {
+    it("shouldn't be actionable - already vaccinated ", async () => {
+      const eligibilityForPerson = await getEligibilityForPerson(VaccineTypes.RSV, "9658218989");
+
+      expect(Object.keys(eligibilityForPerson).length).toEqual(2);
+      expect(eligibilityForPerson.eligibility.status).toEqual(EligibilityStatus.ALREADY_VACCINATED);
+    });
+  });
+
+  describe("Not Eligible", () => {
+    it("shouldn't be eligible ", async () => {
+      const eligibilityForPerson = await getEligibilityForPerson(VaccineTypes.RSV, "9657933617");
+
+      expect(Object.keys(eligibilityForPerson).length).toEqual(2);
+      expect(eligibilityForPerson.eligibility.status).toEqual(EligibilityStatus.NOT_ELIGIBLE);
     });
   });
 });
