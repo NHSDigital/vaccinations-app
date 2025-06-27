ad1401a1d0f124e2bcba1759cc5178d4cb4ba93f
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-06-27 13:42:12 +0100

    VIA-325 AS Remove allowHTMLElement, letting all tags from markdown be displayed

--- src/app/_components/markdown/MarkdownWithStyling.test.tsx
+++ src/app/_components/markdown/MarkdownWithStyling.test.tsx
@@ -2,10 +2,8 @@ import { render, screen } from "@testing-library/react";
 import {
   H2,
   MarkdownWithStyling,
-  allowHTMLElement,
 } from "@src/app/_components/markdown/MarkdownWithStyling";
 import React from "react";
-import { Element as HastElement } from "hast";
 
 let mockedMarkdown: jest.Mock;
 
@@ -39,52 +37,6 @@ describe("markdown", () => {
 
       expect(heading).toBeVisible();
       expect(heading).toHaveClass("nhsuk-heading-s");
-    });
-  });
-
-  describe("allowHTMLElement", () => {
-    const expectedAllowedTags: string[] = [
-      "h1",
-      "h2",
-      "h3",
-      "h4",
-      "h5",
-      "h6",
-      "p",
-      "br",
-      "b",
-      "strong",
-      "em",
-      "i",
-      "a",
-      "ul",
-      "ol",
-      "li",
-    ];
-
-    it.each(expectedAllowedTags)(
-      "should return true for the allowed HTML tag: <%s>",
-      (tagName: string) => {
-        const mockElement: HastElement = {
-          type: "element",
-          tagName: tagName,
-          properties: {},
-          children: [],
-        };
-
-        expect(allowHTMLElement(mockElement)).toBe(true);
-      },
-    );
-
-    it("should return false for disallowed tag", () => {
-      const mockElement: HastElement = {
-        type: "element",
-        tagName: "table",
-        properties: {},
-        children: [],
-      };
-
-      expect(allowHTMLElement(mockElement)).toBe(false);
     });
   });
 });
--- src/app/_components/markdown/MarkdownWithStyling.tsx
+++ src/app/_components/markdown/MarkdownWithStyling.tsx
@@ -1,7 +1,6 @@
 import Markdown from "react-markdown";
 import { HTMLAttributes, ReactNode } from "react";
 import { JSX } from "react/jsx-runtime";
-import { Element } from "hast";
 
 interface MarkdownProps {
   content: string;
@@ -11,32 +10,12 @@ interface H2Props extends HTMLAttributes<HTMLHeadingElement> {
   children?: ReactNode;
 }
 
-const allowedHtmlTagsSet = new Set([
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "p",
-  "br",
-  "b",
-  "strong",
-  "em",
-  "i",
-  "a",
-  "ul",
-  "ol",
-  "li",
-]);
-
 const MarkdownWithStyling = ({ content }: MarkdownProps): JSX.Element => {
   return (
     <Markdown
       components={{
         h2: H2,
       }}
-      allowElement={allowHTMLElement}
     >
       {content}
     </Markdown>
@@ -47,8 +26,4 @@ const H2 = ({ children }: H2Props): JSX.Element => {
   return <h2 className="nhsuk-heading-s">{children}</h2>;
 };
 
-const allowHTMLElement = (element: Element) => {
-  return allowedHtmlTagsSet.has(element.tagName);
-};
-
-export { MarkdownWithStyling, H2, allowHTMLElement };
+export { MarkdownWithStyling, H2 };
