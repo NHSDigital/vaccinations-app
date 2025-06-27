021db9b0a4896d2d640a2451c30171d88a7033e2
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-06-27 13:39:47 +0100

    VIA-325 AS Update E2E tests to login once per eligibility scenario

--- e2e/eligibility.spec.ts
+++ e2e/eligibility.spec.ts
@@ -14,33 +14,42 @@ test.describe("E2E", () => {
     expect(await benchmark(page, RSV_PAGE_URL)).toBeLessThanOrEqual(MAX_AVG_LCP_DURATION_MS);
   });
 
-  test("Not eligible - age and catchup bullet points", async ({ browser }) => {
-    page = await login(browser, users.NotEligible.email);
-    await page.goto(RSV_PAGE_URL);
-    const eligibility: Locator = page.getByTestId("Eligibility");
-    const heading: Locator = eligibility.getByRole("heading", { level: 3, name: "We do not believe you should have this vaccine" });
-    const bulletPoint1: Locator = eligibility.getByText("You are not aged 75 to 79 years old.");
-    const bulletPoint2: Locator = eligibility.getByText("You did not turn 80 between 2nd September 2024 and 31st August 2025");
-    await expect(heading).toBeVisible();
-    await expect(bulletPoint1).toBeVisible();
-    await expect(bulletPoint2).toBeVisible();
+  test.describe("Not Eligible", () => {
+    test.beforeAll(async ({ browser }) => {
+      page = await login(browser, users.NotEligible.email);
+    });
+
+    test("Not eligible - age and catchup bullet points", async () => {
+      await page.goto(RSV_PAGE_URL);
+      const eligibility: Locator = page.getByTestId("Eligibility");
+      const heading: Locator = eligibility.getByRole("heading", { level: 3, name: "We do not believe you should have this vaccine" });
+      const bulletPoint1: Locator = eligibility.getByText("You are not aged 75 to 79 years old.");
+      const bulletPoint2: Locator = eligibility.getByText("You did not turn 80 between 2nd September 2024 and 31st August 2025");
+      await expect(heading).toBeVisible();
+      await expect(bulletPoint1).toBeVisible();
+      await expect(bulletPoint2).toBeVisible();
+    });
   });
 
-  test("Actionable - catchup bullet points", async ({ browser }) => {
-    page = await login(browser, users.Actionable.email);
-    await page.goto(RSV_PAGE_URL);
-    const eligibility: Locator = page.getByTestId("Eligibility");
-    const heading: Locator = eligibility.getByRole("heading", { level: 3, name: "You should have the RSV vaccine" });
-    const bulletPoint: Locator = eligibility.getByText("You are aged 75 to 79 years old.");
-    await expect(heading).toBeVisible();
-    await expect(bulletPoint).toBeVisible();
-  });
+  test.describe("Actionable", () => {
+    test.beforeAll(async ({ browser }) => {
+      page = await login(browser, users.Actionable.email);
+    });
 
-  // TODO: VIA-325 26/06/25 - Check for h2 and paragraph after getting valid markdown from Eligibility API
-  test("Actionable - InfoText action content", async ({ browser}) => {
-    page = await login(browser, users.Actionable.email);
-    await page.goto(RSV_PAGE_URL);
-    const infoText = page.getByText("##Getting the vaccine You can get an RSV vaccination at your GP surgery.");
-    await expect(infoText).toBeVisible();
-  })
+    test("Actionable - catchup bullet points", async () => {
+      await page.goto(RSV_PAGE_URL);
+      const eligibility: Locator = page.getByTestId("Eligibility");
+      const heading: Locator = eligibility.getByRole("heading", { level: 3, name: "You should have the RSV vaccine" });
+      const bulletPoint: Locator = eligibility.getByText("You are aged 75 to 79 years old.");
+      await expect(heading).toBeVisible();
+      await expect(bulletPoint).toBeVisible();
+    });
+
+    // TODO: VIA-325 26/06/25 - Check for h2 and paragraph after getting valid markdown from Eligibility API
+    test("Actionable - InfoText action content", async () => {
+      await page.goto(RSV_PAGE_URL);
+      const infoText = page.getByText("##Getting the vaccine You can get an RSV vaccination at your GP surgery.");
+      await expect(infoText).toBeVisible();
+    });
+  });
 });
