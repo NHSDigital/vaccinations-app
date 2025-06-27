ff6e1633889d7f0fbe3338b6c78220379bda9593
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-06-27 11:59:25 +0100

    VIA-321 Add presence or absence of cohort to EliD contract test

--- contract/eligibility-api.contract.ts
+++ contract/eligibility-api.contract.ts
@@ -10,16 +10,22 @@ describe("EliD API contract", () => {
 
   describe("EliD call over the wire", () => {
     const testCases = [
-      {nhsNumber: "9686368973", expectedStatus: EligibilityStatus.ACTIONABLE},
-      {nhsNumber: "9658218989", expectedStatus: EligibilityStatus.ALREADY_VACCINATED},
-      {nhsNumber: "9657933617", expectedStatus: EligibilityStatus.NOT_ELIGIBLE},
+      {nhsNumber: "9686368973", expectedStatus: EligibilityStatus.ACTIONABLE, expectCohortElement: true},
+      {nhsNumber: "9658218989", expectedStatus: EligibilityStatus.ALREADY_VACCINATED, expectCohortElement: false},
+      {nhsNumber: "9657933617", expectedStatus: EligibilityStatus.NOT_ELIGIBLE, expectCohortElement: true},
     ];
 
-    test.each(testCases)('$nhsNumber should be $expectedStatus', async ({nhsNumber, expectedStatus}) => {
+    test.each(testCases)("$nhsNumber should be $expectedStatus with cohort $expectCohortElement", async ({nhsNumber, expectedStatus, expectCohortElement}) => {
       const eligibilityForPerson = await getEligibilityForPerson(VaccineTypes.RSV, nhsNumber);
 
       expect(Object.keys(eligibilityForPerson).length).toEqual(2);
       expect(eligibilityForPerson.eligibility?.status).toEqual(expectedStatus);
+      if (expectCohortElement) {
+        expect(eligibilityForPerson.eligibility?.content.summary?.cohorts).toBeDefined();
+      } else {
+        expect(eligibilityForPerson.eligibility?.content.summary?.cohorts).toBeUndefined();
+      }
+      expect(eligibilityForPerson.eligibilityError).toBeUndefined();
     });
   });
 });
