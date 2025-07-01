a2f9965722b187bb1d398580015d3bc407c2b707
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-07-01 11:12:26 +0100

    VIA-265 AJ/DB Create SNS topic for cloudwatch alarms

--- infrastructure/modules/deploy_monitoring/alarms.tf
+++ infrastructure/modules/deploy_monitoring/alarms.tf
@@ -4,6 +4,7 @@ module "alarm_server_function_4xx_error" {
 
   alarm_name          = "${var.prefix}-server-function-4xx-error"
   alarm_description   = "Server Lambda: 4xx Errors"
+  # alarm_actions = ["{arn:of:the:sns:topic:to:publish:to}"]
   comparison_operator = "GreaterThanThreshold"
   dimensions = {
     FunctionName = "${var.prefix}-server-function"
@@ -12,6 +13,7 @@ module "alarm_server_function_4xx_error" {
   metric_name         = "Url4xxCount"
   namespace           ="AWS/Lambda"
   period              = 300
+  # ok_actions = {what to do when the alarm does green after being red, optional}
   statistic           = "Sum"
   threshold           = 0
   treat_missing_data  = "notBreaching"
---
+++ infrastructure/modules/deploy_monitoring/sns.tf
@@ -0,0 +1,12 @@
+module "sns" {
+  source  = "terraform-aws-modules/sns/aws"
+  version = "6.2.0"
+
+  name          = "${var.prefix}-cloudwatch-alarms"
+  display_name  = "VitA Cloudwatch Alarms"
+
+  tags  = var.default_tags
+
+  # delivery_policy
+  # subscriptions
+}
