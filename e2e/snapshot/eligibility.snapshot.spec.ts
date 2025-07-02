2e098913a258b0f816bd5a36bbffa2e75f723943
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-07-02 17:51:06 +0100

    VIA-245 AS Update Eligibility snapshot tests to take screenshot of full page

--- e2e/snapshot/eligibility.snapshot.spec.ts
+++ e2e/snapshot/eligibility.snapshot.spec.ts
@@ -1,4 +1,4 @@
-import { expect, Locator, Page, test } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
 import { RSV_PAGE_URL } from "@project/e2e/constants";
 import { login } from "@project/e2e/auth";
 import users from "@test-data/test-users.json" assert { type: "json" };
@@ -15,14 +15,10 @@ test.describe("E2E", () => {
 
     test("Not eligible - age and catchup bullet points", async () => {
       await page.goto(RSV_PAGE_URL);
-      const eligibility: Locator = page.getByTestId("Eligibility");
-      const heading: Locator = eligibility.getByRole("heading", {
-        level: 3,
-        name: "We do not believe you can have it",
-      });
 
-      await expect(heading).toBeVisible();
-      await expect(eligibility).toHaveScreenshot("eligibility-not-eligible.png");
+      await expect(page).toHaveScreenshot("eligibility-not-eligible.png", {
+        fullPage: true,
+      });
     });
   });
 
@@ -33,11 +29,10 @@ test.describe("E2E", () => {
 
     test("Actionable - catchup bullet points", async () => {
       await page.goto(RSV_PAGE_URL);
-      const eligibility: Locator = page.getByTestId("Eligibility");
-      const heading: Locator = eligibility.getByRole("heading", { level: 3, name: "You should have the RSV vaccine" });
 
-      await expect(heading).toBeVisible();
-      await expect(eligibility).toHaveScreenshot("eligibility-actionable.png");
+      await expect(page).toHaveScreenshot("eligibility-actionable.png", {
+        fullPage: true,
+      });
     });
   });
 });
