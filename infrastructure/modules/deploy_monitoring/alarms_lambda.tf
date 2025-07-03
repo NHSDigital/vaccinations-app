265f0ce96e6f49989210a5ec67e60b402e57f754
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-07-03 13:04:51 +0100

    VIA-265 AJ/MD/DB Added lambda error logs to alarm and 5xx errors

--- infrastructure/modules/deploy_monitoring/alarms_lambda.tf
+++ infrastructure/modules/deploy_monitoring/alarms_lambda.tf
@@ -1,8 +1,8 @@
 locals {
-  alarms = {
+  alarms_lambda = {
     "server-lambda-4xx-error" = {
-      alarm_description   = "Server Lambda: 4xx error within last hour"
-      metric_name         = "Url4xxCount"
+      alarm_description = "Server Lambda: 4xx error within last hour"
+      metric_name       = "Url4xxCount"
 
       statistic           = "Sum"
       extended_statistic  = null // as statistic is used
@@ -12,12 +12,12 @@ locals {
 
       period              = 60 // 1min
       evaluation_periods  = 60 // 60 periods
-      datapoints_to_alarm = 1 // number of breaches within the last evaluation period to alarm
+      datapoints_to_alarm = 1  // number of breaches within the last evaluation period to alarm
     },
 
     "server-lambda-uncaught-errors" = {
-      alarm_description   = "Server Lambda: uncaught exception within last hour"
-      metric_name         = "Errors"
+      alarm_description = "Server Lambda: uncaught exception within last hour"
+      metric_name       = "Errors"
 
       statistic           = "Sum"
       extended_statistic  = null // as statistic is used
@@ -27,37 +27,67 @@ locals {
 
       period              = 60 // 1min
       evaluation_periods  = 60 // 60 periods
-      datapoints_to_alarm = 1 // number of breaches within the last evaluation period to alarm
+      datapoints_to_alarm = 1  // number of breaches within the last evaluation period to alarm
     },
 
     "server-lambda-request-latency" = {
-      alarm_description   = "Server Lambda: request latency > 2sec for p99.5 within last hour"
-      metric_name         = "UrlRequestLatency"
+      alarm_description = "Server Lambda: request latency > 2sec for p99.5 within last hour"
+      metric_name       = "UrlRequestLatency"
 
-      statistic           = null  // as extended_statistic is used
+      statistic           = null // as extended_statistic is used
       extended_statistic  = "p99.5"
       comparison_operator = "GreaterThanThreshold"
       threshold           = 2
       unit                = "Seconds"
 
       period              = 600 // 10min
-      evaluation_periods  = 6 // 6 periods
-      datapoints_to_alarm = 1 // number of breaches within the last evaluation period to alarm
-    }
+      evaluation_periods  = 6   // 6 periods
+      datapoints_to_alarm = 1   // number of breaches within the last evaluation period to alarm
+    },
+
+    "server-lambda-5xx-error" = {
+      alarm_description = "Server Lambda: 5xx error within last hour"
+      metric_name       = "Url5xxCount"
+
+      statistic           = "Sum"
+      extended_statistic  = null // as statistic is used
+      comparison_operator = "GreaterThanThreshold"
+      threshold           = 0
+      unit                = "Count"
+
+      period              = 60 // 1min
+      evaluation_periods  = 60 // 60 periods
+      datapoints_to_alarm = 1  // number of breaches within the last evaluation period to alarm
+    },
+
+    "server-lambda-throttles" = {
+      alarm_description = "Server Lambda: concurrent request throttle limit hit within last hour"
+      metric_name       = "Throttles"
+
+      statistic           = "Sum"
+      extended_statistic  = null // as statistic is used
+      comparison_operator = "GreaterThanThreshold"
+      threshold           = 0
+      unit                = "Count"
+
+      period              = 60 // 1min
+      evaluation_periods  = 60 // 60 periods
+      datapoints_to_alarm = 1  // number of breaches within the last evaluation period to alarm
+    },
   }
 }
 
 module "alarms_lambda" {
-  for_each = local.alarms
+  for_each = local.alarms_lambda
 
   source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
   version = "~> 5.7.1"
 
-  alarm_name          = "${var.prefix}-${each.key}"
-  alarm_description   = each.value.alarm_description
+  alarm_name        = "${var.prefix}-${each.key}"
+  alarm_description = each.value.alarm_description
 
-  namespace           = "AWS/Lambda"
-  metric_name         = each.value.metric_name
+  namespace   = "AWS/Lambda"
+  metric_name = each.value.metric_name
 
   statistic           = each.value.statistic
   extended_statistic  = each.value.extended_statistic
@@ -69,9 +99,9 @@ module "alarms_lambda" {
   evaluation_periods  = each.value.evaluation_periods
   datapoints_to_alarm = each.value.datapoints_to_alarm
 
-  alarm_actions       = [module.sns.topic_arn]
-  ok_actions          = [module.sns.topic_arn]
-  treat_missing_data  = "notBreaching"
+  alarm_actions      = [module.sns.topic_arn]
+  ok_actions         = [module.sns.topic_arn]
+  treat_missing_data = "notBreaching"
 
   dimensions = {
     FunctionName = "${var.prefix}-server-function"
---
+++ infrastructure/modules/deploy_monitoring/alarms_lambda_logs.tf
@@ -0,0 +1,87 @@
+locals {
+  alarms_lambda_logs = {
+    "server-lambda-error-logs" = {
+      alarm_description = "Server Lambda: error level log within last hour"
+      metric_name       = "ServerLambdaErrorLogs"
+
+      statistic           = "Sum"
+      comparison_operator = "GreaterThanThreshold"
+      threshold           = 0
+      unit                = "Count"
+
+      period              = 60 // 1min
+      evaluation_periods  = 60
+      datapoints_to_alarm = 1
+      treat_missing_data  = "notBreaching"
+    },
+
+    "content-cache-hydrator-error-logs" = {
+      alarm_description = "Content Cache Hydrator: error level log in last run"
+      metric_name       = "ContentCacheHydratorErrorLogs"
+
+      statistic           = "Sum"
+      comparison_operator = "GreaterThanThreshold"
+      threshold           = 0
+      unit                = "Count"
+
+      period              = 60 // 1min
+      evaluation_periods  = 1  // immediate feedback when turns OK
+      datapoints_to_alarm = 1
+      treat_missing_data  = "ignore" // maintain alarm state
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "server_lambda_error_logs" {
+  name           = "Server lambda error logs"
+  pattern        = "{ $.level = \"ERROR\" }"
+  log_group_name = "/aws/lambda/${var.prefix}-server-function"
+
+  metric_transformation {
+    name      = local.alarms_lambda_logs.server-lambda-error-logs.metric_name
+    namespace = var.prefix
+    value     = "1"
+    unit      = "Count"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "content_cache_hydrator_error_logs" {
+  name           = "Content Cache Hydrator error logs"
+  pattern        = "{ $.level = \"ERROR\" }"
+  log_group_name = "/aws/lambda/${var.prefix}-content-cache-hydrator"
+
+  metric_transformation {
+    name      = local.alarms_lambda_logs.content-cache-hydrator-error-logs.metric_name
+    namespace = var.prefix
+    value     = "1"
+    unit      = "Count"
+  }
+}
+
+module "alarms_logs" {
+  for_each = local.alarms_lambda_logs
+
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+  version = "~> 5.7.1"
+
+  alarm_name        = "${var.prefix}-${each.key}"
+  alarm_description = each.value.alarm_description
+
+  namespace   = var.prefix
+  metric_name = each.value.metric_name
+
+  statistic           = each.value.statistic
+  comparison_operator = each.value.comparison_operator
+  threshold           = each.value.threshold
+  unit                = each.value.unit
+
+  period              = each.value.period
+  evaluation_periods  = each.value.evaluation_periods
+  datapoints_to_alarm = each.value.datapoints_to_alarm
+
+  alarm_actions      = [module.sns.topic_arn]
+  ok_actions         = [module.sns.topic_arn]
+  treat_missing_data = each.value.treat_missing_data
+
+  tags = var.default_tags
+}
