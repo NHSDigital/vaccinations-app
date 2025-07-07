c4f3a7408bc39b05f42710f0b32c44e2d57185c6
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-07-07 10:31:32 +0100

    VIA-245 AS Remove snapshot testing content in Eligibility component

--- src/app/_components/eligibility/Eligibility.tsx
+++ src/app/_components/eligibility/Eligibility.tsx
@@ -36,7 +36,6 @@ const Eligibility = ({ eligibilityContent }: EligibilityProps): JSX.Element => {
           );
         }
       })}
-      <p>This is some content to ensure the correct working of snapshot tests</p>
     </div>
   );
 };
