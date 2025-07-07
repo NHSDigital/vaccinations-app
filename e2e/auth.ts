facb3c1ee84c6726445ac8a76109b7cbb9905789
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-07-07 15:29:18 +0100

    VIA-245 AS Increase wait for time for auth function in e2e tests

--- e2e/auth.ts
+++ e2e/auth.ts
@@ -59,7 +59,7 @@ export const login = async (browser: Browser, nhsLoginUsername: string): Promise
   await page.getByRole("textbox", { name: "Security code" }).fill(user.nhsLoginOTP);
   await page.getByRole("button", { name: "Continue" }).click();
 
-  await page.waitForURL(user.vaccinationsHubUrl, { timeout: 30000 });
+  await page.waitForURL(user.vaccinationsHubUrl, { timeout: 60000 });
 
   return page;
 };
