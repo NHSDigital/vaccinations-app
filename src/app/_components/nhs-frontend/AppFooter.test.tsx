72f8742cea6d75acf262dd64ea410c89f091acf8
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-07-08 17:50:43 +0100

    VIA-216, VIA-217, VIA-218, VIA-387 AJ Added footer links

--- src/app/_components/nhs-frontend/AppFooter.test.tsx
+++ src/app/_components/nhs-frontend/AppFooter.test.tsx
@@ -2,9 +2,37 @@ import AppFooter from "@src/app/_components/nhs-frontend/AppFooter";
 import { render, screen } from "@testing-library/react";
 
 describe("AppFooter", () => {
-  it("renders crown copyright", async () => {
+  beforeEach(() => {
     render(<AppFooter />);
+  });
+
+  it("renders crown copyright", async () => {
     const copyright = screen.queryByText(/© Crown copyright/);
     expect(copyright).toBeVisible();
+  });
+
+  it("renders terms of use", async () => {
+    const termsOfUse = screen.queryByText(/Terms of use/);
+    expect(termsOfUse).toBeVisible();
+  });
+
+  it("renders privacy policy", async () => {
+    const privacyPolicy = screen.queryByText(/Privacy policy/);
+    expect(privacyPolicy).toBeVisible();
+  });
+
+  it("renders help and support", async () => {
+    const helpAndSupport = screen.queryByText(/Help and support/);
+    expect(helpAndSupport).toBeVisible();
+  });
+
+  it("renders accessibility statement", async () => {
+    const accessibilityStatement = screen.queryByText(/Accessibility statement/);
+    expect(accessibilityStatement).toBeVisible();
+  });
+
+  it("renders cookies policy", async () => {
+    const cookiesPolicy = screen.queryByText(/Cookies/);
+    expect(cookiesPolicy).toBeVisible();
   });
 });
--- src/app/_components/nhs-frontend/AppFooter.tsx
+++ src/app/_components/nhs-frontend/AppFooter.tsx
@@ -6,7 +6,20 @@ import React from "react";
 const AppFooter = () => {
   return (
     <Footer>
-      <Footer.List></Footer.List>
+      <Footer.List>
+        <Footer.ListItem href="/our-policies/terms">Terms of use</Footer.ListItem>
+        <Footer.ListItem
+          href="https://www.england.nhs.uk/contact-us/privacy-notice/national-flu-vaccination-programme/"
+          target={"_blank"}
+        >
+          Privacy policy
+        </Footer.ListItem>
+        <Footer.ListItem href="https://www.nhs.uk/nhs-app/nhs-app-help-and-support/" target={"_blank"}>
+          Help and support
+        </Footer.ListItem>
+        <Footer.ListItem href="/our-policies/accessibility">Accessibility statement</Footer.ListItem>
+        <Footer.ListItem href="/our-policies/cookies-policy">Cookies</Footer.ListItem>
+      </Footer.List>
       <Footer.Copyright>© Crown copyright</Footer.Copyright>
     </Footer>
   );
