a46d33d42d11e477eeef946c8a9a54c4afb093c9
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-07-18 16:42:05 +0100

    VIA-404 AS/DB Use new vita test users for e2e tests
    # Conflicts:
    #	test-data/test-users.json

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -2,12 +2,18 @@ import { TestInfo, test as setup } from "@playwright/test";
 import { login } from "@project/e2e/auth";
 import users from "@test-data/test-users.json" with { type: "json" };
 
+const testUserPattern = process.env.VITA_TEST_USER_PATTERN;
+if (!testUserPattern) {
+  throw new Error("VITA_TEST_USER_PATTERN environment variable is not defined.");
+}
+
 for (const scenario in users) {
   const key = scenario as keyof typeof users;
 
   setup(`authenticate ${key}`, async ({ browser }, testInfo: TestInfo) => {
     testInfo.setTimeout(60000);
-    const userEmail = users[key].email;
+
+    const userEmail = testUserPattern.replace("{id}", users[key].user_identifier);
     const authFile = `./e2e/.auth/${key}.json`;
 
     const page = await login(browser, userEmail);
