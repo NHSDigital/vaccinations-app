a6fe93f9c19cce1ac7e30abcf5540bb6f70cbf68
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-07-23 16:12:30 +0100

    VIA-283 AS/DB Setup configuration for Prod env

---
+++ infrastructure/environments/prod/ssm.tf
@@ -0,0 +1,27 @@
+resource "aws_ssm_parameter" "nhs_uk_content_api_key" {
+  name             = "/${local.prefix}/CONTENT_API_KEY"
+  type             = "SecureString"
+  value_wo         = "to-be-replaced-manually"
+  value_wo_version = 0
+}
+
+resource "aws_ssm_parameter" "nhs_login_client_id" {
+  name             = "/${local.prefix}/NHS_LOGIN_CLIENT_ID"
+  type             = "SecureString"
+  value_wo         = "to-be-replaced-manually"
+  value_wo_version = 0
+}
+
+resource "aws_ssm_parameter" "nhs_login_private_key" {
+  name             = "/${local.prefix}/NHS_LOGIN_PRIVATE_KEY"
+  type             = "SecureString"
+  value_wo         = "to-be-replaced-manually"
+  value_wo_version = 0
+}
+
+resource "aws_ssm_parameter" "eligibility_api_key" {
+  name             = "/${local.prefix}/ELIGIBILITY_API_KEY"
+  type             = "SecureString"
+  value_wo         = "to-be-replaced-manually"
+  value_wo_version = 0
+}
