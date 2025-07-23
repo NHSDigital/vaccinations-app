37fdaf88e0b2f850455ad123af1abbe2049d02f2
Simon Brunning <196635190+acerathereinspirative-nhs-nvir@users.noreply.github.com>
2025-07-23 14:03:19 +0100

    VIA-394 SB Add "How to turn on JavaScript" link.

--- src/app/_components/static/NoJsMessage.tsx
+++ src/app/_components/static/NoJsMessage.tsx
@@ -8,6 +8,11 @@ export function NoJsMessage() {
           <div className="nhsuk-grid-column-full">
             <h1 className="app-dynamic-page-title__heading">Cannot show page</h1>
             <p>You need to turn on JavaScript to access NHS App services in your browser.</p>
+            <p>
+              <a href="https://www.nhs.uk/nhs-app/nhs-app-help-and-support/nhs-app-technical-information/technical-issues-with-the-nhs-app/">
+                How to turn on JavaScript
+              </a>
+            </p>
           </div>
         </div>
       </main>
