de45c9964b7fd5870de207301205f052400e4f4b
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-07-28 12:14:58 +0100

    Reapply "VIA-254 AJ/MD/DB Add APIM auth request generator methods"
    This reverts commit 2f29980ffede7700aa14c43eb62ae86b41bc0830.

--- src/utils/auth/generate-auth-payload.ts
+++ src/utils/auth/generate-auth-payload.ts
@@ -1,42 +1,29 @@
 import { auth } from "@project/auth";
-import { AssertedLoginIdentityPayload, CommonAuthPayload, RefreshClientAssertionPayload } from "@src/utils/auth/types";
+import { DecodedIdToken } from "@src/utils/auth/types";
 import { AppConfig } from "@src/utils/config";
 import jwt from "jsonwebtoken";
+import { jwtDecode } from "jwt-decode";
 
 const ASSERTED_LOGIN_IDENTITY_EXPIRY_SECONDS = 60;
 
 const generateAssertedLoginIdentityJwt = async (config: AppConfig): Promise<string> => {
   const session = await auth();
-  const jtiFromIdToken = session?.user.id_token.jti;
 
-  if (!jtiFromIdToken) {
-    throw new Error("Error creating SSO assertedLoginIdentity: id_token.jti attribute missing from session");
+  if (!session?.nhs_login?.id_token) {
+    throw new Error(
+      `Missing information. hasSession=${!!session}, hasNHSLogin=${!!session?.nhs_login}, hasIDToken=${!!session?.nhs_login?.id_token}`,
+    );
   }
 
-  const assertedLoginIdentityPayload = {
-    code: jtiFromIdToken,
-  };
+  const jtiFromIdToken = jwtDecode<DecodedIdToken>(session.nhs_login.id_token).jti;
 
-  return generateSignedJwtWith(config, assertedLoginIdentityPayload, ASSERTED_LOGIN_IDENTITY_EXPIRY_SECONDS);
-};
-
-const generateSignedJwtWith = async (
-  config: AppConfig,
-  extraPayloadFields: RefreshClientAssertionPayload | AssertedLoginIdentityPayload,
-  expiryTimeSeconds: number,
-) => {
-  const now = Math.floor(Date.now() / 1000);
-
-  const commonAuthPayload: CommonAuthPayload = {
+  const nowInSeconds: number = Math.floor(Date.now() / 1000);
+  const payload = {
     iss: config.NHS_LOGIN_CLIENT_ID,
     jti: crypto.randomUUID(),
-    exp: now + expiryTimeSeconds,
-    iat: now,
-  };
-
-  const payload = {
-    ...commonAuthPayload,
-    ...extraPayloadFields,
+    code: jtiFromIdToken,
+    exp: nowInSeconds + ASSERTED_LOGIN_IDENTITY_EXPIRY_SECONDS,
+    iat: nowInSeconds,
   };
 
   return jwt.sign(payload, config.NHS_LOGIN_PRIVATE_KEY, { algorithm: "RS512" });
