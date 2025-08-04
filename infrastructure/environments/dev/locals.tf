54aa4db619cd9e1b8dad7caca977675a1f6d5269
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-08-04 16:28:28 +0100

    VIA-286 AJ/AS debugging pino (temporary log level to warn)

--- infrastructure/environments/dev/locals.tf
+++ infrastructure/environments/dev/locals.tf
@@ -21,7 +21,7 @@ locals {
   application_environment_variables = {
     SSM_PREFIX = "/${local.prefix}/"
 
-    PINO_LOG_LEVEL      = "info"
+    PINO_LOG_LEVEL      = "warn"
     DEPLOY_ENVIRONMENT  = local.environment
     PROFILE_PERFORMANCE = "true"
 
