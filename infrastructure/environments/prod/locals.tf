b26c265db8ab94ff9061bb7bf74eec57611b311f
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-08-05 09:52:56 +0100

    AJ formatting and fixed log level for sandpit

--- infrastructure/environments/prod/locals.tf
+++ infrastructure/environments/prod/locals.tf
@@ -35,7 +35,7 @@ locals {
     MAX_SESSION_AGE_MINUTES = 59
 
     ELIGIBILITY_API_ENDPOINT = "https://api.service.nhs.uk/"
-    IS_APIM_AUTH_ENABLED        = false
+    IS_APIM_AUTH_ENABLED     = false
     APIM_AUTH_URL            = ""
     APIM_KEY_ID              = ""
 
