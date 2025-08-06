8728ab58c513bbd915c17b462a3cda4db39b8e59
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-08-06 17:59:28 +0100

    VIA-394 AJ/SB Open the guidance in new tab instead of same

--- src/app/_components/static/NoJsMessage.tsx
+++ src/app/_components/static/NoJsMessage.tsx
@@ -9,7 +9,11 @@ export function NoJsMessage() {
             <h1 className="app-dynamic-page-title__heading">Cannot show page</h1>
             <p>You need to turn on JavaScript to access NHS App services in your browser.</p>
             <p>
-              <a href="https://www.nhs.uk/nhs-app/nhs-app-help-and-support/nhs-app-technical-information/technical-issues-with-the-nhs-app/">
+              <a
+                href="https://www.nhs.uk/nhs-app/nhs-app-help-and-support/nhs-app-technical-information/technical-issues-with-the-nhs-app/"
+                target="_blank"
+                rel="noopener"
+              >
                 How to turn on JavaScript
               </a>
             </p>
