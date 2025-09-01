d9726c1d5b04363e22ed15d93120079cbac64198
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-09-01 16:30:49 +0100

    VIA-347 AS/SB/AJ Temporarily remove ACM certificate filter

--- infrastructure/environments/test/acm.tf
+++ infrastructure/environments/test/acm.tf
@@ -2,8 +2,8 @@ data "aws_acm_certificate" "website" {
   provider    = aws.global
   domain      = format("*.%s", local.domain)
   key_types   = ["RSA_2048"]
-  statuses    = ["ISSUED"]
-  types       = ["AMAZON_ISSUED"]
+  # statuses    = ["ISSUED"]
+  # types       = ["AMAZON_ISSUED"]
   most_recent = true
   tags = {
     Project = local.project_identifier
