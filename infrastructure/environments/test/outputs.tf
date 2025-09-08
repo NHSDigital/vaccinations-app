db970b429d083d577593eaa77e11d83249b0dbe3
Simon Brunning <196635190+acerathereinspirative-nhs-nvir@users.noreply.github.com>
2025-09-08 13:20:26 +0100

    VIA-345 SB Correct terraform outputs.

--- infrastructure/environments/test/outputs.tf
+++ infrastructure/environments/test/outputs.tf
@@ -2,3 +2,13 @@ output "cloudfront_url" {
   value       = module.deploy.cloudfront_url
   description = "The URL for the cloudfront distribution"
 }
+
+output "application_url" {
+  description = "The public URL of the fake-api application."
+  value       = module.deploy_fake_api.application_url
+}
+
+output "fake_api_ecr_repository_url" {
+  description = "The ECR repository URL for the fake-api service."
+  value       = module.deploy_fake_api.fake_api_ecr_repository_url
+}
