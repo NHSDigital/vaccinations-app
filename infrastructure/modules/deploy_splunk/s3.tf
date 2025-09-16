d70dc2e2e14bdeb318d51fb532f23c646b20084e
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-09-16 16:24:30 +0100

    VIA-433 AS/AJ Add delete lifecycle rule to firehose failed logs s3 bucket

--- infrastructure/modules/deploy_splunk/s3.tf
+++ infrastructure/modules/deploy_splunk/s3.tf
@@ -6,3 +6,19 @@ module "failed_firehose_delivery_logs_s3_bucket" {
   force_destroy = false
   tags          = var.default_tags
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "failed_firehose_delivery_logs_s3_bucket_lifecycle_configuration" {
+  bucket = module.failed_firehose_delivery_logs_s3_bucket.s3_bucket_id
+
+  rule {
+    id = "DeleteLogsFilesAfterExpiration"
+
+    filter {}
+
+    expiration {
+      days = var.splunk_log_retention_in_days
+    }
+
+    status = "Enabled"
+  }
+}
