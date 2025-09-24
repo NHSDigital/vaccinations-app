b0070ef566fe0325178f5f1a616b5989d6cdae63
Marie Dedikova <197628467+marie-dedikova-nhs@users.noreply.github.com>
2025-09-24 11:48:26 +0100

    VIA-236 MD: Add missing functionality to click on items in ContentList & fix the position of the cookie table

--- src/app/_components/nhs-frontend/ContentsList.tsx
+++ src/app/_components/nhs-frontend/ContentsList.tsx
@@ -1,19 +1,38 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
 
 interface ContentsListProps {
   contents: string[];
 }
 
 const ContentsList = (props: ContentsListProps): JSX.Element => {
+  const [clickedItem, setClickedItem] = useState("");
+
+  function handleClick(item: string) {
+    setClickedItem(item);
+  }
+
   return (
     <nav className="nhsuk-contents-list" role="navigation" aria-label="Pages in this guide">
       <h2 className="nhsuk-u-visually-hidden">Contents</h2>
       <ol className="nhsuk-contents-list__list">
-        {props.contents.map((contentItem: string, index: number) => (
-          <li className="nhsuk-contents-list__item" key={index}>
-            <span className="nhsuk-contents-list__link">{contentItem}</span>
-          </li>
-        ))}
+        {props.contents.map((contentItem: string, index: number) =>
+          clickedItem === contentItem ? (
+            <li
+              className="nhsuk-contents-list__item"
+              key={index}
+              aria-current="page"
+              onClick={() => handleClick(contentItem)}
+            >
+              <span className="nhsuk-contents-list__link__current">{contentItem}</span>
+            </li>
+          ) : (
+            <li className="nhsuk-contents-list__item" key={index} onClick={() => handleClick(contentItem)}>
+              <a href={`#${contentItem.toLowerCase()}`} className="nhsuk-contents-list__link">
+                {contentItem}
+              </a>
+            </li>
+          ),
+        )}
       </ol>
     </nav>
   );
--- src/app/our-policies/cookies-policy/page.tsx
+++ src/app/our-policies/cookies-policy/page.tsx
@@ -37,8 +37,8 @@ const CookiesPolicy = () => {
           <li>strictly necessary cookies on your device to make this service work</li>
         </ul>
         <h2>Strictly necessary cookies</h2>
-        <h2>Changes to cookies</h2>
         <Details title={"List of necessary cookies that make this service work"} component={<CookiesTable />} />
+        <h2>Changes to cookies</h2>
         <p>
           Our cookie policy may change. The latest version of our cookie policy will be accessible through this service.
           We will inform you if we make any material changes to our cookies policy or privacy notice. This will allow
