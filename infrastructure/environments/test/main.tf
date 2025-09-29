1b4b05a25d43c8fea2f90bc2fbf5a032da0aaeeb
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-09-29 14:41:47 +0100

    VIA-373 AJ Update other environments as per dev

--- infrastructure/environments/test/main.tf
+++ infrastructure/environments/test/main.tf
@@ -1,14 +1,3 @@
-module "deploy_iam" {
-  source = "../../modules/deploy_iam"
-
-  account_id        = data.aws_caller_identity.current.account_id
-  prefix            = local.prefix
-  region            = local.region
-  environment       = local.environment
-  project_shortcode = local.project_identifier_shortcode
-  is_local          = !var.is_github_action
-}
-
 module "deploy_lambda" {
   source = "../../modules/deploy_lambda"
 
