919fdb427d6dc2cf8494f6c6d76f69bf2013ec6f
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-09-29 12:02:49 +0100

    VIA-373 AJ Split IAM deployment and the app to resolve catch-22 issue

---
+++ infrastructure/environments/shared/main.tf
@@ -0,0 +1,10 @@
+module "deploy_iam" {
+  source = "../../modules/deploy_iam"
+
+  account_id        = data.aws_caller_identity.current.account_id
+  prefix            = local.prefix
+  region            = local.region
+  environment       = local.environment
+  project_shortcode = local.project_identifier_shortcode
+  is_local          = !var.is_github_action
+}
---
+++ infrastructure/environments/shared/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "vaccinations-app-tfstate-dev"
+    key    = "iam/terraform.tfstate"
+    region = "eu-west-2"
+
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = local.region
+  default_tags {
+    tags = local.default_tags
+  }
+}
