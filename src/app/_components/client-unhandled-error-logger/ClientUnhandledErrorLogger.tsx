30a250969df91e687b78d957063f1205c56618ee
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-09-30 13:41:13 +0100

    VIA-357 Ensure cleanup of client side error logging when component unmounts

--- src/app/_components/client-unhandled-error-logger/ClientUnhandledErrorLogger.tsx
+++ src/app/_components/client-unhandled-error-logger/ClientUnhandledErrorLogger.tsx
@@ -4,13 +4,20 @@ import logClientSideError from "@src/utils/client-side-error-logger/client-side-
 import { ClientSideErrorTypes } from "@src/utils/constants";
 import { useEffect } from "react";
 
+const reportClientSideError = () => {
+  logClientSideError(ClientSideErrorTypes.UNHANDLED_ERROR).catch(() => {
+    // do not show anything to the user; catching prevents an infinite loop if the logger itself throws an error which is unhandled
+  });
+};
+
 const ClientUnhandledErrorLogger = (): undefined => {
   useEffect(() => {
-    window.addEventListener("unhandledrejection", function () {
-      logClientSideError(ClientSideErrorTypes.UNHANDLED_ERROR).catch(() => {
-        // do not show anything to the user; catching prevents an infinite loop if the logger itself throws an error which is unhandled
-      });
-    });
+    window.addEventListener("unhandledrejection", reportClientSideError);
+
+    // on component unmount
+    return () => {
+      window.removeEventListener("unhandledrejection", reportClientSideError);
+    };
   }, []);
 };
 
