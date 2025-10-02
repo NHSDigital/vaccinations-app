ad3b4e67f0afaffbc0203b42d4faceef2d18793f
Marie Dedikova <197628467+marie-dedikova-nhs@users.noreply.github.com>
2025-10-02 16:41:20 +0100

    VIA-335: MD Add missing e2e tests to check NBS buttons for user 19 and for pregnancy page

--- e2e/general/nbs.spec.ts
+++ e2e/general/nbs.spec.ts
@@ -1,5 +1,12 @@
 import { Browser, BrowserContext, Locator, expect, test } from "@playwright/test";
-import { BOOKING_PAGE_TITLE, BOOKING_PAGE_URL, RSV_PAGE_TITLE, RSV_PAGE_URL } from "@project/e2e/constants";
+import {
+  BOOKING_PAGE_TITLE,
+  BOOKING_PAGE_URL,
+  RSV_PAGE_TITLE,
+  RSV_PAGE_URL,
+  RSV_PREGNANCY_PAGE_TITLE,
+  RSV_PREGNANCY_PAGE_URL,
+} from "@project/e2e/constants";
 import { getEnv } from "@project/e2e/helpers";
 
 const loadNBSAuthEnvironmentVariables = () => {
@@ -22,7 +29,47 @@ export const loginToNBS = async (browser: Browser) => {
   return { page: await nbsContext.newPage(), context: nbsContext };
 };
 
-test.describe.configure({ mode: "parallel", retries: 3 });
+test.describe.configure({ mode: "parallel", retries: 0 });
+
+test.describe("NBS booking redirection - user 19", () => {
+  test.use({ storageState: "./e2e/.auth/actionable-with-booking-link.json" });
+
+  test("link to book appointment at pharmacy on RSV page for older adults", async ({ browser }) => {
+    const { page, context } = await loginToNBS(browser);
+
+    await page.goto(RSV_PAGE_URL);
+    await expect(page).toHaveTitle(RSV_PAGE_TITLE);
+
+    const bookingLink = page.getByRole("link", { name: "book an RSV vaccination in a pharmacy" });
+    await expect(bookingLink).toBeVisible();
+
+    const [newPage] = await Promise.all([context.waitForEvent("page"), bookingLink.click()]);
+
+    await newPage.waitForLoadState("domcontentloaded");
+    await newPage.bringToFront();
+
+    await expect(newPage).toHaveTitle(BOOKING_PAGE_TITLE);
+    expect(newPage.url()).toContain(BOOKING_PAGE_URL);
+  });
+
+  test("link to book appointment at pharmacy on RSV page for pregnant people", async ({ browser }) => {
+    const { page, context } = await loginToNBS(browser);
+
+    await page.goto(RSV_PREGNANCY_PAGE_URL);
+    await expect(page).toHaveTitle(RSV_PREGNANCY_PAGE_TITLE);
+
+    const bookingLink = page.getByRole("link", { name: "book an RSV vaccination in a pharmacy" });
+    await expect(bookingLink).toBeVisible();
+
+    const [newPage] = await Promise.all([context.waitForEvent("page"), bookingLink.click()]);
+
+    await newPage.waitForLoadState("domcontentloaded");
+    await newPage.bringToFront();
+
+    await expect(newPage).toHaveTitle(BOOKING_PAGE_TITLE);
+    expect(newPage.url()).toContain(BOOKING_PAGE_URL);
+  });
+});
 
 test.describe("NBS booking redirection - user 21", () => {
   test.use({ storageState: "./e2e/.auth/actionable-with-booking-button.json" });
@@ -37,6 +84,24 @@ test.describe("NBS booking redirection - user 21", () => {
     await expect(bookingButton).toBeVisible();
 
     const [newPage] = await Promise.all([context.waitForEvent("page"), bookingButton.click()]);
+
+    await newPage.waitForLoadState("domcontentloaded");
+    await newPage.bringToFront();
+
+    await expect(newPage).toHaveTitle(BOOKING_PAGE_TITLE);
+    expect(newPage.url()).toContain(BOOKING_PAGE_URL);
+  });
+
+  test("link to book appointment at pharmacy on RSV page for pregnant people", async ({ browser }) => {
+    const { page, context } = await loginToNBS(browser);
+
+    await page.goto(RSV_PREGNANCY_PAGE_URL);
+    await expect(page).toHaveTitle(RSV_PREGNANCY_PAGE_TITLE);
+
+    const bookingLink = page.getByRole("link", { name: "book an RSV vaccination in a pharmacy" });
+    await expect(bookingLink).toBeVisible();
+
+    const [newPage] = await Promise.all([context.waitForEvent("page"), bookingLink.click()]);
 
     await newPage.waitForLoadState("domcontentloaded");
     await newPage.bringToFront();
@@ -61,6 +126,24 @@ test.describe("NBS booking redirection - user 22", () => {
     await expect(manageButton).toBeVisible();
 
     const [newPage] = await Promise.all([context.waitForEvent("page"), manageButton.click()]);
+
+    await newPage.waitForLoadState("domcontentloaded");
+    await newPage.bringToFront();
+
+    await expect(newPage).toHaveTitle(BOOKING_PAGE_TITLE);
+    expect(newPage.url()).toContain(BOOKING_PAGE_URL);
+  });
+
+  test("link to book appointment at pharmacy on RSV page for pregnant people", async ({ browser }) => {
+    const { page, context } = await loginToNBS(browser);
+
+    await page.goto(RSV_PREGNANCY_PAGE_URL);
+    await expect(page).toHaveTitle(RSV_PREGNANCY_PAGE_TITLE);
+
+    const bookingLink = page.getByRole("link", { name: "book an RSV vaccination in a pharmacy" });
+    await expect(bookingLink).toBeVisible();
+
+    const [newPage] = await Promise.all([context.waitForEvent("page"), bookingLink.click()]);
 
     await newPage.waitForLoadState("domcontentloaded");
     await newPage.bringToFront();
