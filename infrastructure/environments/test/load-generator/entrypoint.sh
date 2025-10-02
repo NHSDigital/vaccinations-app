14e6258f3c1cca58956eb0a17f1b25601cde7290
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-10-02 17:20:10 +0100

    VIA-373 AJ Avoid result file name collisions

--- infrastructure/environments/test/load-generator/entrypoint.sh
+++ infrastructure/environments/test/load-generator/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-set -ex
+set -e
 
 # Define variables
-RESULT_FILE=result-$(date +%Y%m%d%H%M%S).jtl
+RESULTS_PATH=$(date +%Y)/$(date +%m)/$(date +%d)
+RESULT_FILE=$(date +%H_%M_%S_%3N)_$RANDOM.jtl
 
 # Download the JMeter test plan from S3
 echo "Downloading test plan from S3..."
 aws s3 cp "s3://${S3_BUCKET}/plans/${TEST_PLAN}" /opt/jmeter/test_plan.jmx
+echo "Downloaded."
 
 # Run JMeter test
 echo "Running JMeter test..."
@@ -18,9 +20,11 @@ jmeter -n \
   -JEnvironment="${ENVIRONMENT}" \
   -JBaseLocation="/opt/jmeter" \
   -l "/opt/jmeter/${RESULT_FILE}"
+echo "Done."
 
 # Upload the results back to S3
 echo "Uploading results to S3..."
-aws s3 cp "/opt/jmeter/${RESULT_FILE}" "s3://${S3_BUCKET}/results/"
+aws s3 cp "/opt/jmeter/${RESULT_FILE}" "s3://${S3_BUCKET}/results/${RESULTS_PATH}/"
+echo "Uploaded."
 
-echo "Test execution completed."
+echo "Load test completed successfully."
