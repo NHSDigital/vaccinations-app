e5a56baa1e227c69c12806ab8dbb0f18e612b4fd
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-10-08 14:17:32 +0100

    VIA-474 Add cross browser testing support
    - Add separate cross browser testing playwright config file
    - Update shceduled assurance run to run with latest tagged release in preprod
    - Update other related workflows

---
+++ playwright.config.cross-browser.ts
@@ -0,0 +1,148 @@
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import type { GitHubActionOptions } from "@estruyf/github-actions-reporter";
+
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, ".env.local") });
+
+export const snapshotPathTemplate =
+  "{testDir}/snapshot/__snapshots__/{testFileName}-snapshots/{arg}-{projectName}-{platform}{ext}";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 10000,
+  },
+
+  timeout: 60000,
+
+  testDir: "./e2e",
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 3 : 3,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI
+    ? [
+      ["line"],
+      ["html"],
+      [
+        "@estruyf/github-actions-reporter",
+        <GitHubActionOptions>{
+          title: "E2E Test Results",
+          useDetails: true,
+          showError: true,
+          showTags: false,
+        },
+      ],
+    ]
+    : "html",
+  snapshotPathTemplate,
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: process.env.TEST_APP_URL,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "setup",
+      testMatch: /.*\.setup\.ts/,
+    },
+    {
+      name: "Chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1920, height: 1080 },
+      },
+      dependencies: ['setup']
+    },
+    {
+      name: "Edge",
+      use: {
+        ...devices["Desktop Edge"],
+        viewport: { width: 1920, height: 1080 },
+      },
+      dependencies: ['setup']
+    },
+    {
+      name: "Firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        viewport: { width: 1920, height: 1080 },
+      },
+      dependencies: ['setup']
+    },
+    {
+      name: "Safari",
+      use: {
+        ...devices["Desktop Safari"],
+        viewport: { width: 1920, height: 1080 },
+      },
+      dependencies: ['setup']
+    },
+    {
+      name: 'ChromeAndroid',
+      use: { ...devices['Pixel 5'] },
+      dependencies: ['setup']
+    },
+    {
+      name: 'SafariiPhone',
+      use: { ...devices['iPhone 12'] },
+      dependencies: ['setup']
+    },
+    {
+      name: 'SamsungInternetAndroid',
+      use: { ...devices['Pixel 5'] },
+      dependencies: ['setup']
+    },
+    {
+      name: 'ChromeiPhone',
+      use: { ...devices['iPhone 12'] },
+      dependencies: ['setup']
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
