f00f4a957409048a56688856f80508abdeda4502
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-11-06 10:43:17 +0000

    VIA-568 AJ/MD Add side effects to the snapshots

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -88,7 +88,7 @@ export const pathForCustomScreenshots = (testFileName: string, screenshotFileNam
 };
 
 export const openExpanders = async (page: Page) => {
-  const expanderTitles = ["What this vaccine is for", "Who should have this vaccine"];
+  const expanderTitles = ["What this vaccine is for", "Who should have this vaccine", "Side effects of the vaccine"];
 
   for (const title of expanderTitles) {
     await page.getByText(title).click();
