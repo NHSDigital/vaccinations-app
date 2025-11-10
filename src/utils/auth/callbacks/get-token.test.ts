12d2ff0cb2d841e616b58a908f1bddca99e6ca09
Donna Belsey <196086361+donna-belsey-nhs@users.noreply.github.com>
2025-11-10 19:15:12 +0000

    TASK Sonar fix: reduce duplication in getToken tests

--- src/utils/auth/callbacks/get-token.test.ts
+++ src/utils/auth/callbacks/get-token.test.ts
@@ -44,10 +44,25 @@ describe("getToken", () => {
 
   const nowInSeconds = 1749052001;
 
+  const profile = {
+    nhs_number: "test_nhs_number",
+  };
+
+  const account = {
+    expires_at: nowInSeconds + 1000,
+    access_token: "newAccess",
+    refresh_token: "newRefresh",
+    id_token: "newIdToken",
+  } as Account;
+
   beforeEach(() => {
     jest.clearAllMocks();
     jest.useFakeTimers().setSystemTime(nowInSeconds * 1000);
     process.env.NEXT_RUNTIME = "nodejs";
+
+    (jwtDecode as jest.Mock).mockReturnValue({
+      jti: "jti_test",
+    });
   });
 
   afterEach(() => {
@@ -73,68 +88,32 @@ describe("getToken", () => {
     });
 
     it("should return updated token on initial login with account profile, and APIM credentials", async () => {
-      // Given
-      (jwtDecode as jest.Mock).mockReturnValue({
-        jti: "jti_test",
-      });
       const token = { apim: {}, nhs_login: { id_token: "id-token" } } as JWT;
-      const account = {
-        expires_at: nowInSeconds + 1000,
-        access_token: "newAccess",
-        refresh_token: "newRefresh",
-        id_token: "newIdToken",
-      } as Account;
-      const profile = {
-        nhs_number: "test_nhs_number",
-      };
       const maxAgeInSeconds = 600 as MaxAgeInSeconds;
 
-      // When
       const result = await getToken(token, account, profile, mockConfig, maxAgeInSeconds);
 
-      // Then
-      expect(result).toMatchObject({
-        user: {
-          nhs_number: profile.nhs_number,
-        },
-        nhs_login: {
-          id_token: "newIdToken",
-        },
-        apim: {
-          access_token: "new-apim-access-token",
-          expires_at: nowInSeconds + 1111,
-        },
-        fixedExpiry: nowInSeconds + maxAgeInSeconds,
-      });
+      expectResultToMatchTokenWith(
+        result,
+        profile.nhs_number,
+        "newIdToken",
+        "new-apim-access-token",
+        nowInSeconds + 1111,
+        maxAgeInSeconds,
+      );
     });
 
     // todo: check the apim assertion still holds
     it("should return token with empty values on initial login if account and profile are undefined", async () => {
-      const token = {} as JWT;
-
-      const account = {} as Account;
-
-      const profile = {} as Profile;
-
+      const undefinedToken = {} as JWT;
+      const undefinedAccount = {} as Account;
+      const undefinedProfile = {} as Profile;
       const maxAgeInSeconds = 600 as MaxAgeInSeconds;
-
       (getOrRefreshApimCredentials as jest.Mock).mockResolvedValue(undefined);
 
-      const result = await getToken(token, account, profile, mockConfig, maxAgeInSeconds);
+      const result = await getToken(undefinedToken, undefinedAccount, undefinedProfile, mockConfig, maxAgeInSeconds);
 
-      expect(result).toMatchObject({
-        user: {
-          nhs_number: "",
-        },
-        nhs_login: {
-          id_token: "",
-        },
-        apim: {
-          access_token: "",
-          expires_at: 0,
-        },
-        fixedExpiry: nowInSeconds + maxAgeInSeconds,
-      });
+      expectResultToMatchTokenWith(result, "", "", "", 0, maxAgeInSeconds);
     });
 
     it("should fill in missing values in token with default empty string", async () => {
@@ -176,39 +155,13 @@ describe("getToken", () => {
     it("should still return login token even if fetching APIM credentials fails", async () => {
       (getOrRefreshApimCredentials as jest.Mock).mockRejectedValue(new ApimHttpError("Error getting APIM token"));
 
-      (jwtDecode as jest.Mock).mockReturnValue({
-        jti: "jti_test",
-      });
       const token = { apim: {}, nhs_login: { id_token: "id-token" } } as JWT;
-
-      const account = {
-        expires_at: nowInSeconds + 1000,
-        access_token: "newAccess",
-        refresh_token: "newRefresh",
-        id_token: "newIdToken",
-      } as Account;
-
-      const profile = {
-        nhs_number: "test_nhs_number",
-      };
 
       const maxAgeInSeconds = 600 as MaxAgeInSeconds;
 
       const result = await getToken(token, account, profile, mockConfig, maxAgeInSeconds);
 
-      expect(result).toMatchObject({
-        user: {
-          nhs_number: profile.nhs_number,
-        },
-        nhs_login: {
-          id_token: "newIdToken",
-        },
-        apim: {
-          access_token: "",
-          expires_at: 0,
-        },
-        fixedExpiry: nowInSeconds + maxAgeInSeconds,
-      });
+      expectResultToMatchTokenWith(result, profile.nhs_number, "newIdToken", "", 0, maxAgeInSeconds);
     });
   });
 
@@ -218,39 +171,37 @@ describe("getToken", () => {
     });
 
     it("should return updated token on initial login with account profile, and default empty APIM credentials", async () => {
-      (jwtDecode as jest.Mock).mockReturnValue({
-        jti: "jti_test",
-      });
       const token = { apim: {}, nhs_login: { id_token: "id-token" } } as JWT;
-
-      const account = {
-        expires_at: nowInSeconds + 1000,
-        access_token: "newAccess",
-        refresh_token: "newRefresh",
-        id_token: "newIdToken",
-      } as Account;
-
-      const profile = {
-        nhs_number: "test_nhs_number",
-      };
 
       const maxAgeInSeconds = 600 as MaxAgeInSeconds;
 
       const result = await getToken(token, account, profile, mockConfig, maxAgeInSeconds);
 
-      expect(result).toMatchObject({
-        user: {
-          nhs_number: profile.nhs_number,
-        },
-        nhs_login: {
-          id_token: "newIdToken",
-        },
-        apim: {
-          access_token: "",
-          expires_at: 0,
-        },
-        fixedExpiry: nowInSeconds + maxAgeInSeconds,
-      });
+      expectResultToMatchTokenWith(result, profile.nhs_number, "newIdToken", "", 0, maxAgeInSeconds);
     });
   });
+
+  const expectResultToMatchTokenWith = (
+    result: JWT | null,
+    nhsNumber: string,
+    idToken: string,
+    apimToken: string,
+    apimExpiresAt: number,
+    maxAgeInSeconds: number,
+  ) => {
+    expect(result).not.toBeNull();
+    expect(result).toMatchObject({
+      user: {
+        nhs_number: nhsNumber,
+      },
+      nhs_login: {
+        id_token: idToken,
+      },
+      apim: {
+        access_token: apimToken,
+        expires_at: apimExpiresAt,
+      },
+      fixedExpiry: nowInSeconds + maxAgeInSeconds,
+    });
+  };
 });
