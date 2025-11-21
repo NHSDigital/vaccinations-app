06d4131bf792f9104c512f0848960d45981c02d0
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-11-21 15:04:03 +0000

    VIA-608 AJ/DB Remove bottom margin from urgent and non-urgent care cards

--- src/app/_components/nhs-frontend/NonUrgentCareCard.tsx
+++ src/app/_components/nhs-frontend/NonUrgentCareCard.tsx
@@ -4,6 +4,8 @@ import { Heading } from "@src/services/eligibility-api/types";
 import { Card } from "nhsuk-react-components";
 import { JSX } from "react";
 
+import styles from "./styles.module.css";
+
 interface NonUrgentCareCardProps {
   heading: Heading | string | JSX.Element;
   content: JSX.Element;
@@ -13,7 +15,7 @@ const NonUrgentCareCard = ({ heading, content }: NonUrgentCareCardProps) => {
   return (
     <Card cardType="non-urgent" data-testid="non-urgent-care-card">
       <Card.Heading>{heading}</Card.Heading>
-      <Card.Content>{content}</Card.Content>
+      <Card.Content className={styles.careCardZeroMarginBottom}>{content}</Card.Content>
     </Card>
   );
 };
