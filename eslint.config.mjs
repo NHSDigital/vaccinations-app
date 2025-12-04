1066a22515235dc097ae5bc1d63d4b0cc0390b2c
Anoop Surej <201928812+anoop-surej-nhs@users.noreply.github.com>
2025-12-04 17:09:59 +0000

    VIA-579 AS/AJ Don't allow any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = defineConfig([
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "error",
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
