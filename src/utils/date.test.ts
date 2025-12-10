1c56153e422af63cdde3343831ef3896ff72c105
Simon Brunning <196635190+acerathereinspirative-nhs-nvir@users.noreply.github.com>
2025-12-10 16:01:30 +0000

    CHORE SB Shakes fist at Sonar.

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -2,131 +2,64 @@ import { UtcDateFromStringSchema, UtcDateTimeFromStringSchema } from "@src/utils
 
 describe("utils-date", () => {
   describe("UtcDateFromStringSchema", () => {
-    it("should parse winter date", () => {
-      const actual = UtcDateFromStringSchema.parse("2026-01-01");
-
-      expect(actual).toEqual(new Date("2026-01-01"));
+    it.each([
+      { description: "winter date", input: "2026-01-01" },
+      { description: "summer date", input: "2026-06-01" },
+    ])("should parse $description", ({ input }) => {
+      const actual = UtcDateFromStringSchema.parse(input);
+      expect(actual).toEqual(new Date(input));
     });
 
-    it("should parse summer date", () => {
-      const actual = UtcDateFromStringSchema.parse("2026-06-01");
-
-      expect(actual).toEqual(new Date("2026-06-01"));
-    });
-
-    it("should throw on invalid date", () => {
-      const given = "2026-13-01";
-
+    it.each([
+      { description: "invalid date", input: "2026-13-01" },
+      { description: "date without dashes", input: "20261301" },
+      { description: "badly formed date", input: "Sausages" },
+    ])("should throw on $description", ({ input }) => {
       expect(() => {
-        UtcDateFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on date without dashes", () => {
-      const given = "20261301";
-
-      expect(() => {
-        UtcDateFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on badly formed date", () => {
-      const given = "Sausages";
-
-      expect(() => {
-        UtcDateFromStringSchema.parse(given);
+        UtcDateFromStringSchema.parse(input);
       }).toThrow();
     });
   });
 
-  describe("UtcDateTimeFromStringSchema with Zulu time", () => {
-    it("should parse winter date", () => {
-      const actual = UtcDateTimeFromStringSchema.parse("2026-01-01T13:16:02Z");
-
-      expect(actual).toEqual(new Date("2026-01-01T13:16:02.000Z"));
+  describe("UtcDateTimeFromStringSchema", () => {
+    it.each([
+      {
+        description: "winter date (Zulu)",
+        input: "2026-01-01T13:16:02Z",
+        expected: "2026-01-01T13:16:02.000Z",
+      },
+      {
+        description: "summer date (Zulu)",
+        input: "2026-06-01T13:16:02Z",
+        expected: "2026-06-01T13:16:02.000Z",
+      },
+      {
+        description: "winter date (Offset)",
+        input: "2026-01-01T13:16:02+02:00",
+        expected: "2026-01-01T11:16:02.000Z",
+      },
+      {
+        description: "summer date (Offset)",
+        input: "2026-06-01T13:16:02+02:00",
+        expected: "2026-06-01T11:16:02.000Z",
+      },
+    ])("should parse $description", ({ input, expected }) => {
+      const actual = UtcDateTimeFromStringSchema.parse(input);
+      expect(actual).toEqual(new Date(expected));
     });
 
-    it("should parse summer date", () => {
-      const actual = UtcDateTimeFromStringSchema.parse("2026-06-01T13:16:02Z");
-
-      expect(actual).toEqual(new Date("2026-06-01T13:16:02.000Z"));
-    });
-
-    it("should throw on invalid date", () => {
-      const given = "2026-13-01T13:16:02Z";
-
+    it.each([
+      { description: "invalid date (Zulu)", input: "2026-13-01T13:16:02Z" },
+      { description: "invalid time (Zulu)", input: "2026-12-01T25:16:02Z" },
+      { description: "date without dashes (Zulu)", input: "20261301T13:16:02Z" },
+      { description: "badly formed string (Zulu)", input: "SausagesT13:16:02Z" },
+      { description: "invalid date (Offset)", input: "2026-13-01T13:16:02+02:00" },
+      { description: "invalid time (Offset)", input: "2026-12-01T25:16:02+02:00" },
+      { description: "date without dashes (Offset)", input: "20261301T13:16:02+02:00" },
+      { description: "badly formed string (Offset)", input: "SausagesT13:16:02+02:00" },
+    ])("should throw on $description", ({ input }) => {
       expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on invalid time", () => {
-      const given = "2026-12-01T25:16:02Z";
-
-      expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on date without dashes", () => {
-      const given = "20261301T13:16:02Z";
-
-      expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on badly formed date", () => {
-      const given = "SausagesT13:16:02Z";
-
-      expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
-      }).toThrow();
-    });
-  });
-
-  describe("UtcDateTimeFromStringSchema with offset time", () => {
-    it("should parse winter date", () => {
-      const actual = UtcDateTimeFromStringSchema.parse("2026-01-01T13:16:02+02:00");
-
-      expect(actual).toEqual(new Date("2026-01-01T11:16:02.000Z"));
-    });
-
-    it("should parse summer date", () => {
-      const actual = UtcDateTimeFromStringSchema.parse("2026-06-01T13:16:02+02:00");
-
-      expect(actual).toEqual(new Date("2026-06-01T11:16:02.000Z"));
-    });
-
-    it("should throw on invalid date", () => {
-      const given = "2026-13-01T13:16:02+02:00";
-
-      expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on invalid time", () => {
-      const given = "2026-12-01T25:16:02+02:00";
-
-      expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on date without dashes", () => {
-      const given = "20261301T13:16:02+02:00";
-
-      expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
-      }).toThrow();
-    });
-
-    it("should throw on badly formed date", () => {
-      const given = "SausagesT13:16:02+02:00";
-
-      expect(() => {
-        UtcDateTimeFromStringSchema.parse(given);
+        UtcDateTimeFromStringSchema.parse(input);
       }).toThrow();
     });
   });
