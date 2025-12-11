5d5f14d513fa0debb047a7b200b167c69ff34c82
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2025-12-11 18:19:57 +0000

    VIA-579 AJ Add supported browsers list to warn when using old apis

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -14,10 +14,14 @@ const copyExtraFiles = async () => {
   // there is an issue with esbuild putting a warning for xhr-sync-worker.js file being external
   // at runtime, lambda complains that the file is not present
   // DOMPurify apparently depends on this file
-  fs.copyFile("./node_modules/jsdom/lib/jsdom/living/xhr/xhr-sync-worker.js", `${OUTPUT_DIR}/xhr-sync-worker.js`, (err) => {
-    if (err) throw err;
-    console.log("Copied xhr-sync-worker.js (workaround)");
-  });
+  fs.copyFile(
+    "./node_modules/jsdom/lib/jsdom/living/xhr/xhr-sync-worker.js",
+    `${OUTPUT_DIR}/xhr-sync-worker.js`,
+    (err) => {
+      if (err) throw err;
+      console.log("Copied xhr-sync-worker.js (workaround)");
+    },
+  );
 };
 
 const buildLambda = async () => {
@@ -29,7 +33,7 @@ const buildLambda = async () => {
     jsx: "automatic",
     target: "node22",
     external: ["./xhr-sync-worker.js"],
-    outfile: `${OUTPUT_DIR}/lambda.js`
+    outfile: `${OUTPUT_DIR}/lambda.js`,
   });
 };
 
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import compat_plugin from "eslint-plugin-compat";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,32 +10,45 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [{
-  ignores: [
-    "node_modules/**",
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-    "public/**",
-    "dist/**",
-    "docs/**",
-    "coverage/**",
-    "**/.terraform/**",
-    ".open-next/**",
-    "playwright-report/**",
-    "pact/**",
-    "performance/report",
-  ]
-}, ...compat.extends(
-  "next/core-web-vitals",
-  "next/typescript",
-  "prettier",
-  "next",
-), {
-  "rules": {
-    "@typescript-eslint/no-unused-vars": "error",
-  }
-}];
+const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "public/**",
+      "dist/**",
+      "docs/**",
+      "coverage/**",
+      "**/.terraform/**",
+      ".open-next/**",
+      "playwright-report/**",
+      "pact/**",
+      "performance/report",
+    ],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier", "next"),
+  compat_plugin.configs["flat/recommended"],
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+
+      // nhsuk-frontend: https://github.com/nhsuk/nhsuk-frontend/blob/main/docs/contributing/browser-support.md
+      // Ref: https://github.com/nhsuk/nhsuk-frontend/blob/main/packages/nhsuk-frontend/.browserslistrc
+      // supported browsers are listed in package.json
+      "compat/compat": "warn",
+    },
+  },
+
+  // Override for test files: turn off compat
+  {
+    files: ["**/*.test.{js,jsx,ts,tsx}", "**/*.spec.{js,jsx,ts,tsx}", "test-data/**"],
+    rules: {
+      "compat/compat": "off",
+    },
+  },
+];
 
 export default eslintConfig;
