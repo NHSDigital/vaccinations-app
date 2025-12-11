a3b3c1cf8fafe743bdb0fbb4b60a688b18884e25
Marie Dedikova <197628467+marie-dedikova-nhs@users.noreply.github.com>
2025-12-11 17:50:26 +0000

    VIA-630 MD/DB: Move calculateAge to the file date and its test file

--- src/utils/age.ts
+++
@@ -1,9 +0,0 @@
-import { UtcDateFromStringSchema } from "@src/utils/date";
-import { differenceInYears } from "date-fns";
-
-export const calculateAge = (date: string): number => {
-  const today: Date = new Date();
-  const birthDate: Date = UtcDateFromStringSchema.parse(date);
-
-  return differenceInYears(today, birthDate);
-};
