a95195cd1ca7c840cdab61d17a038dba0d5d4caf
Ankur Jain <174601014+ankur-jain-nhs@users.noreply.github.com>
2026-03-27 18:53:32 +0000

    AJ Add visual logs viewer with timeline

---
+++ logs-viewer/types.ts
@@ -0,0 +1,32 @@
+interface LogEntry {
+  result: {
+    time?: number;
+    timestamp?: string;
+    _time?: string;
+    traceId?: string;
+    sessionId?: string;
+    module?: string;
+    message?: any;
+    msg?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+interface LogNode {
+  id: string;
+  label: string;
+  millisSinceEpoch: number;
+  color: string;
+  font?: object;
+  shape: "circle" | "box" | "ellipse";
+  info: object;
+  module: string;
+  context: string;
+  device: object | undefined;
+  size: number;
+  x: number;
+  y: number;
+}
+
+export type { LogEntry, LogNode };
